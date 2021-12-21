9ac8ca377221a6b79ae2bdd234c7ba9a28925138
Phani Kumar <phani.kumar@astronomer.io>
2021-12-21 12:43:38 +0530

    modified test

--- tests/astronomer_operators/hooks/test_postgres_async.py
+++ tests/astronomer_operators/hooks/test_postgres_async.py
@@ -7,29 +7,21 @@ from astronomer_operators.hooks.postgres import PostgresHookAsync
 
 
 def test_rowcount():
-    print("$$$$$$$$$$")
-    print("Inside test_rowcount")
     os.environ[
         "AIRFLOW_CONN_POSTGRES_DEFAULT"
     ] = "postgresql://postgres:@localhost/airflow"
     loop = asyncio.get_event_loop()
     result = loop.run_until_complete(rowcount())
     loop.close()
-    print("Result is")
-    print(result)
-    print("$$$$$$$$$$")
-    assert result is None
+    assert result["status"] == "success"
+    assert result["message"] == "CREATE TABLE"
 
 
 @pytest.mark.asyncio
 async def rowcount():
-    print("###########")
-    print("Inside test_rowcount_async")
     hook = PostgresHookAsync()
     print(hook.__dict__)
     response = await hook.run(
         sql="CREATE TABLE IF NOT EXISTS test_postgres_hook_table (c VARCHAR)"
     )
-    print("###########")
-    print("Response is..")
-    print(response)
+    return response
