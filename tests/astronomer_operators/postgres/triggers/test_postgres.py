44efc6bd5b8c1e646136b4700276863177bef560
Phani Kumar <phani.kumar@astronomer.io>
2021-12-21 16:00:56 +0530

    Resolve static failures

---
+++ tests/astronomer_operators/postgres/triggers/test_postgres.py
@@ -0,0 +1,48 @@
+import asyncio
+import logging
+
+import pytest
+
+from astronomer_operators.postgres.triggers.postgres import PostgresTrigger
+
+TASK_ID = "postgres_check"
+CONN_ID = "postgres_default"
+
+
+def test_postgres_trigger_serialization():
+    """
+    Asserts that the PostgresTrigger correctly serializes its arguments
+    and classpath.
+    """
+    trigger = PostgresTrigger(task_id=TASK_ID, sql="SELECT 1", postgres_conn_id=CONN_ID)
+    classpath, kwargs = trigger.serialize()
+    assert classpath == "astronomer_operators.postgres.triggers.postgres.PostgresTrigger"
+    assert kwargs == {
+        "sql": "SELECT 1",
+        "task_id": TASK_ID,
+        "postgres_conn_id": CONN_ID,
+        "database": None,
+        "parameters": None,
+        "poll_interval": 1,
+    }
+
+
+@pytest.mark.asyncio
+async def test_postgres_trigger_running(caplog):
+    """
+    Tests the PostgresTrigger run state
+    """
+    caplog.set_level(logging.INFO)
+
+    trigger = PostgresTrigger(postgres_conn_id=CONN_ID, task_id=TASK_ID, sql="SELECT 1")
+
+    task = asyncio.create_task(trigger.run().__anext__())
+    await asyncio.sleep(1)
+
+    # TriggerEvent was returned
+    assert task.done() is True
+
+    assert "Connecting to postgres" in caplog.text
+
+    # Prevents error when task is destroyed while in "pending" state
+    asyncio.get_event_loop().stop()
