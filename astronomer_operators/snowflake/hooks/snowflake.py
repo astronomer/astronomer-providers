a34311cfe5c1e65adc211ebe95bd1849f8ff49c0
Bharanidharan <bharanidharan.manimuthu@astronomer.io>
2021-12-24 17:26:28 +0530

    Removed unused import

--- astronomer_operators/snowflake/hooks/snowflake.py
+++ astronomer_operators/snowflake/hooks/snowflake.py
@@ -1,24 +1,23 @@
 import asyncio
 from contextlib import closing
 from io import StringIO
-from typing import List, Optional, Union
-from snowflake.connector import DictCursor
-from snowflake.connector.util_text import split_statements
-from snowflake.connector import ProgrammingError
-from snowflake.connector.constants import QueryStatus
+from typing import Optional, Union
+
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from asgiref.sync import sync_to_async
+from snowflake.connector import DictCursor, ProgrammingError
+from snowflake.connector.constants import QueryStatus
+from snowflake.connector.util_text import split_statements
 
 
 class SnowflakeHookAsync(SnowflakeHook):
-
     def run(self, sql: Union[str, list], autocommit: bool = False, parameters: Optional[dict] = None):
         """
         Runs a command or a list of commands. Pass a list of sql
         statements to the sql parameter to get them to execute
         sequentially. The variable query_ids is returned so that
         it can be used to check the  modify the behavior
-        depending on the result of the query 
+        depending on the result of the query
         :param sql: the sql string to be executed with possibly multiple statements,
           or a list of sql statements to execute
         :type sql: str or list
@@ -57,26 +56,26 @@ class SnowflakeHookAsync(SnowflakeHook):
             if not self.get_autocommit(conn):
                 conn.commit()
         self.log.info("query ids: ", self.query_ids)
-        return self.query_ids        
-    
+        return self.query_ids
+
     def check_query_output(self, query_ids):
         """
-        Once the qurey is finished fetch the result and log it in airflow 
+        Once the qurey is finished fetch the result and log it in airflow
         """
         with closing(self.get_conn()) as conn:
             with closing(conn.cursor(DictCursor)) as cur:
                 for query_id in query_ids:
                     cur.get_results_from_sfqid(query_id)
                     results = cur.fetchall()
-                    print(f'{results[0]}')
+                    print(f"{results[0]}")
                     self.log.info("Rows affected: %s", cur.rowcount)
                     self.log.info("Snowflake query id: %s", query_id)
 
     async def get_query_status(self, query_ids):
         """
         Async function to get the Query status by query Ids, this function takes list of query_ids make
-        sync_to_async connection 
-        to snowflake to get the query status by query id returns the query status. 
+        sync_to_async connection
+        to snowflake to get the query status by query id returns the query status.
         """
         try:
             sfqid = []
@@ -91,19 +90,28 @@ class SnowflakeHookAsync(SnowflakeHook):
                             self.log.info("The query finished successfully")
                             sfqid.append(query_id)
                         elif status == QueryStatus.ABORTING:
-                            return {"status": "error", "message": 
-                            "The query is in the process of being aborted on the server side.", "type": "ABORTING", "query_id": query_id}
+                            return {
+                                "status": "error",
+                                "message": "The query is in the process of being aborted on the server side.",
+                                "type": "ABORTING",
+                                "query_id": query_id,
+                            }
                         elif status == QueryStatus.FAILED_WITH_ERROR:
-                            return {"status": "error", "message":
-                            "The query finished unsuccessfully.", "type": "FAILED_WITH_ERROR", "query_id": query_id}
+                            return {
+                                "status": "error",
+                                "message": "The query finished unsuccessfully.",
+                                "type": "FAILED_WITH_ERROR",
+                                "query_id": query_id,
+                            }
                         else:
                             return status
                     return {"status": "success", "query_ids": sfqid}
             except ProgrammingError as err:
-                error_message =  'Programming Error: {0}'.format(err)
+                error_message = "Programming Error: {0}".format(err)
                 self.log.error("error_message ", error_message)
                 return {"status": "error", "message": error_message, "type": "ERROR"}
         except Exception as e:
+            print(e)
             self.log.error(e)
             return {"status": "error", "message": e, "type": "ERROR"}
 
