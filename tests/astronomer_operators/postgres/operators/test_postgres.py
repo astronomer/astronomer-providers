86524f88598af12e0ec1c27a74352f24b0af147b
Phani Kumar <phani.kumar@astronomer.io>
2022-01-04 17:37:43 +0530

    add more tests for operator

--- tests/astronomer_operators/postgres/operators/test_postgres.py
+++ tests/astronomer_operators/postgres/operators/test_postgres.py
@@ -1,7 +1,7 @@
 from unittest import mock
 
 import pytest
-from airflow.exceptions import TaskDeferred
+from airflow.exceptions import AirflowException, AirflowNotFoundException, TaskDeferred
 
 from astronomer_operators.postgres.operators.postgres import PostgresOperatorAsync
 from astronomer_operators.postgres.triggers.postgres import PostgresTrigger
@@ -18,6 +18,18 @@ def context():
     """
     context = {}
     yield context
+
+
+def test_postgres_operator_async_missing_connection(context):
+    """
+    Checks that AirflowNotFoundException is raised when there is no connection available
+    """
+    op = PostgresOperatorAsync(task_id=TASK_ID, postgres_conn_id=CONN_ID, sql="SELECT 1")
+
+    with pytest.raises(AirflowNotFoundException) as exc:
+        op.execute(context)
+
+    assert exc.value.title == f"The conn_id `{CONN_ID}` isn't defined"
 
 
 @mock.patch("astronomer_operators.postgres.operators.postgres._PostgresHook.run")
@@ -46,3 +58,11 @@ def test_postgres_execute_complete():
     mock_log_info.assert_called_with(
         "%s completed successfully with response %s ", "run_now", "Query Execution completed. "
     )
+
+
+def test_postgres_execute_failure(context):
+    """Tests that an AirflowException is raised in case of error event"""
+    operator = PostgresOperatorAsync(task_id="run_now", postgres_conn_id=CONN_ID, sql="SELECT 1")
+
+    with pytest.raises(AirflowException):
+        operator.execute_complete(context=None, event={"status": "error", "message": "test failure message"})
