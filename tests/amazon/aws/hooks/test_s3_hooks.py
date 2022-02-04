f0627068e0165d186c7a29fcb085455d3cc6eb95
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-02-05 00:13:29 +0545

    Add tests for S3KeySensorAsync Hooks

--- tests/amazon/aws/hooks/test_s3_hooks.py
+++ tests/amazon/aws/hooks/test_s3_hooks.py
@@ -9,7 +9,16 @@ from astronomer_operators.amazon.aws.hooks.base_aws_async import AwsBaseHookAsyn
 
 
 @pytest.mark.asyncio
-async def test_aws_base_hook_async_get_client_async():
+async def test_aws_base_hook_async_get_client_async_without_get_connection():
+    aws_base_hook_async_obj = AwsBaseHookAsync(client_type="S3", resource_type="S3")
+    response = await aws_base_hook_async_obj.get_client_async()
+
+    assert isinstance(response, ClientCreatorContext)
+
+
+@mock.patch("astronomer_operators.amazon.aws.hooks.base_aws_async.AwsBaseHookAsync.get_connection")
+@pytest.mark.asyncio
+async def test_aws_base_hook_async_get_client_async_with_get_connection(mock_connection):
     aws_base_hook_async_obj = AwsBaseHookAsync(client_type="S3", resource_type="S3")
     response = await aws_base_hook_async_obj.get_client_async()
 
