349fc43581d4417c585217309f05525309220c81
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-02-05 00:08:08 +0545

    Add tests for S3KeySensorAsync Hooks

--- tests/amazon/aws/hooks/test_s3_hooks.py
+++ tests/amazon/aws/hooks/test_s3_hooks.py
@@ -1,6 +1,9 @@
-import asyncio
+import json
+from unittest import mock
 
 import pytest
+from aiobotocore.session import ClientCreatorContext
+from airflow.models.connection import Connection
 
 from astronomer_operators.amazon.aws.hooks.base_aws_async import AwsBaseHookAsync
 
@@ -8,7 +11,18 @@ from astronomer_operators.amazon.aws.hooks.base_aws_async import AwsBaseHookAsyn
 @pytest.mark.asyncio
 async def test_aws_base_hook_async_get_client_async():
     aws_base_hook_async_obj = AwsBaseHookAsync(client_type="S3", resource_type="S3")
-    task = asyncio.create_task(aws_base_hook_async_obj.get_client_async())
+    response = await aws_base_hook_async_obj.get_client_async()
 
-    await asyncio.sleep(0.5)
-    assert task.done() is True
+    assert isinstance(response, ClientCreatorContext)
+
+
+@mock.patch("astronomer_operators.amazon.aws.hooks.base_aws_async.AwsBaseHookAsync.get_connection")
+@pytest.mark.asyncio
+async def test_aws_base_hook_async_get_client_async_with_aws_secrets(mock_get_connection):
+    mock_conn = Connection(extra=json.dumps({"aws_access_key_id": "", "aws_secret_access_key": ""}))
+    mock_get_connection.return_value = mock_conn
+
+    aws_base_hook_async_obj = AwsBaseHookAsync(client_type="S3", resource_type="S3")
+    response = await aws_base_hook_async_obj.get_client_async()
+
+    assert isinstance(response, ClientCreatorContext)
