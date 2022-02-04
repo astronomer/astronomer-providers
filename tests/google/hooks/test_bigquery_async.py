91e1da5a185db3e5855b42b135fcd9f9810610ea
phanikumv <phani.kumar@astronomer.io>
2022-02-04 22:14:19 +0530

    add more tests for big query async hook

--- tests/google/hooks/test_bigquery_async.py
+++ tests/google/hooks/test_bigquery_async.py
@@ -23,7 +23,6 @@ TABLE_REFERENCE_REPR = {
 @pytest.mark.asyncio
 @mock.patch("astronomer_operators.google.hooks.bigquery_async.Session")
 async def test_get_job_instance(mock_session):
-    print("Entered test_get_job_instance")
     hook = BigQueryHookAsync()
     result = await hook.get_job_instance(project_id=PROJECT_ID, job_id=JOB_ID, session=mock_session)
     assert isinstance(result, Job)
@@ -32,11 +31,30 @@ async def test_get_job_instance(mock_session):
 @pytest.mark.asyncio
 @mock.patch("astronomer_operators.google.hooks.bigquery_async.BigQueryHookAsync.get_job_instance")
 @mock.patch("astronomer_operators.google.hooks.bigquery_async.Session")
-async def test_get_job_status(mock_session, mock_job_instance):
-    print("Entered test_get_job_status")
+async def test_get_job_status_success(mock_session, mock_job_instance):
     hook = BigQueryHookAsync()
     resp = await hook.get_job_status(job_id=JOB_ID, project_id=PROJECT_ID)
-    print(resp)
+    assert resp == "success"
     mock_job_instance.assert_called_once_with(
         PROJECT_ID, JOB_ID, mock_session.return_value.__aenter__.return_value
     )
+
+
+@pytest.mark.asyncio
+@mock.patch("astronomer_operators.google.hooks.bigquery_async.BigQueryHookAsync.get_job_instance")
+async def test_get_job_status_oserror(mock_job_instance):
+    """Assets that the BigQueryHookAsync returns a pending response when OSError is raised"""
+    mock_job_instance.return_value.result.side_effect = OSError()
+    hook = BigQueryHookAsync()
+    job_status = await hook.get_job_status(job_id=JOB_ID, project_id=PROJECT_ID)
+    assert job_status == "pending"
+
+
+@pytest.mark.asyncio
+@mock.patch("astronomer_operators.google.hooks.bigquery_async.BigQueryHookAsync.get_job_instance")
+async def test_get_job_status_exception(mock_job_instance, caplog):
+    """Assets that the logging is done correctly when BigQueryHookAsync raises Exception"""
+    mock_job_instance.return_value.result.side_effect = Exception()
+    hook = BigQueryHookAsync()
+    await hook.get_job_status(job_id=JOB_ID, project_id=PROJECT_ID)
+    assert "Query execution finished with errors..." in caplog.text
