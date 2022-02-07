c4d27bdacfd171e3aa279640a6c07549abdba0cb
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-02-07 15:12:28 +0545

    Add tests for S3 Trigger for execptions

--- tests/amazon/aws/triggers/test_s3_triggers.py
+++ tests/amazon/aws/triggers/test_s3_triggers.py
@@ -1,7 +1,9 @@
 import asyncio
+import unittest
 from unittest import mock
 
 import pytest
+from botocore.exceptions import ClientError
 
 from astronomer_operators.amazon.aws.triggers.s3 import S3KeyTrigger
 
@@ -72,3 +74,55 @@ async def test_s3_key_trigger_check_exact_key(mock_client):
     task = await trigger._check_exact_key(mock_client.return_value, "s3://test_bucket/file", "test_bucket")
 
     assert task is True
+
+
+@mock.patch("astronomer_operators.amazon.aws.triggers.s3.S3HookAsync.get_client_async")
+@pytest.mark.asyncio
+async def test_s3_key_trigger_check_key_with_error(mock_client):
+    trigger = S3KeyTrigger(bucket_key="s3://test_bucket/file", bucket_name="test_bucket")
+
+    mock_client.head_object.side_effect = ClientError(
+        {
+            "Error": {
+                "Code": "SomeServiceException",
+                "Message": "Details/context around the exception or error",
+            },
+            "ResponseMetadata": {
+                "RequestId": "1234567890ABCDEF",
+                "HostId": "host ID data will appear here as a hash",
+                "HTTPStatusCode": 404,
+                "HTTPHeaders": {"header metadata key/values will appear here"},
+                "RetryAttempts": 0,
+            },
+        },
+        operation_name="s3",
+    )
+    response = await trigger._check_exact_key(mock_client, "s3://test_bucket/file", "test_bucket")
+    assert response is False
+
+
+@mock.patch("astronomer_operators.amazon.aws.triggers.s3.S3HookAsync.get_client_async")
+@pytest.mark.asyncio
+@unittest.expectedFailure
+async def test_s3_key_trigger_check_key_raise_exception(mock_client):
+    trigger = S3KeyTrigger(bucket_key="s3://test_bucket/file", bucket_name="test_bucket")
+
+    mock_client.head_object.side_effect = ClientError(
+        {
+            "Error": {
+                "Code": "SomeServiceException",
+                "Message": "Details/context around the exception or error",
+            },
+            "ResponseMetadata": {
+                "RequestId": "1234567890ABCDEF",
+                "HostId": "host ID data will appear here as a hash",
+                "HTTPStatusCode": 500,
+                "HTTPHeaders": {"header metadata key/values will appear here"},
+                "RetryAttempts": 0,
+            },
+        },
+        operation_name="s3",
+    )
+    with pytest.raises(ClientError) as err:
+        response = await trigger._check_exact_key(mock_client, "s3://test_bucket/file", "test_bucket")
+        assert isinstance(response, err)
