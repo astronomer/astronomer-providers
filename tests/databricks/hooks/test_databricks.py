5686b53d497845a531bbb4721c309da2971b0b2c
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-02-09 04:42:10 +0545

    Fix pre-commit black error

--- tests/databricks/hooks/test_databricks.py
+++ tests/databricks/hooks/test_databricks.py
@@ -4,10 +4,12 @@ from unittest.mock import MagicMock
 
 import pytest
 from airflow.exceptions import AirflowException
-from airflow.providers.databricks.hooks.databricks import __version__ as provider_version
 from airflow.providers.databricks.hooks.databricks import (
     GET_RUN_ENDPOINT,
     SUBMIT_RUN_ENDPOINT,
+)
+from airflow.providers.databricks.hooks.databricks import (
+    __version__ as provider_version,
 )
 from packaging import version
 
