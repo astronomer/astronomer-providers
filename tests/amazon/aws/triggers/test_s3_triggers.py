a38fa4256d708fbc7df4835df9e1a5a11fd06552
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-02-10 05:43:59 +0545

    Add docstring for tests in triggerers

--- tests/amazon/aws/triggers/test_s3_triggers.py
+++ tests/amazon/aws/triggers/test_s3_triggers.py
@@ -29,6 +29,10 @@ def test_s3_key_trigger_serialization():
 @mock.patch("astronomer_operators.amazon.aws.triggers.s3.S3HookAsync.get_client_async")
 @mock.patch("astronomer_operators.amazon.aws.triggers.s3.S3KeyTrigger._check_key")
 async def test_s3_key_trigger_run(mock_check_key, mock_client):
+    """
+    Test if the task is run is in triggerr successfully.
+    :return:
+    """
     mock_check_key.return_value = True
     trigger = S3KeyTrigger(bucket_key="s3://test_bucket/file", bucket_name="test_bucket")
     with mock_client:
@@ -44,6 +48,10 @@ async def test_s3_key_trigger_run(mock_check_key, mock_client):
 @mock.patch("astronomer_operators.amazon.aws.triggers.s3.S3KeyTrigger._check_wildcard_key")
 @pytest.mark.asyncio
 async def test_s3_key_trigger_check_key(mock_check_wildcard_key, mock_check_exact_key, mock_client):
+    """
+    Test for key with and without wildcard.
+    :return:
+    """
     trigger = S3KeyTrigger(bucket_key="s3://test_bucket/file", bucket_name="test_bucket")
 
     # test for wildcard=False
@@ -70,6 +78,10 @@ async def test_s3_key_trigger_check_key(mock_check_wildcard_key, mock_check_exac
 @mock.patch("astronomer_operators.amazon.aws.triggers.s3.S3HookAsync.get_client_async")
 @pytest.mark.asyncio
 async def test_s3_key_trigger_check_exact_key(mock_client):
+    """
+    Test if the key exists and asserts True if key is found.
+    :return:
+    """
     trigger = S3KeyTrigger(bucket_key="s3://test_bucket/file", bucket_name="test_bucket")
     task = await trigger._check_exact_key(mock_client.return_value, "s3://test_bucket/file", "test_bucket")
 
@@ -79,6 +91,10 @@ async def test_s3_key_trigger_check_exact_key(mock_client):
 @mock.patch("astronomer_operators.amazon.aws.triggers.s3.S3HookAsync.get_client_async")
 @pytest.mark.asyncio
 async def test_s3_key_trigger_check_key_with_error(mock_client):
+    """
+    Test for 404 error if key not found and assert based on response.
+    :return:
+    """
     trigger = S3KeyTrigger(bucket_key="s3://test_bucket/file", bucket_name="test_bucket")
 
     mock_client.head_object.side_effect = ClientError(
@@ -105,6 +121,10 @@ async def test_s3_key_trigger_check_key_with_error(mock_client):
 @pytest.mark.asyncio
 @unittest.expectedFailure
 async def test_s3_key_trigger_check_key_raise_exception(mock_client):
+    """
+    Test for 500 error if key not found and assert based on response.
+    :return:
+    """
     trigger = S3KeyTrigger(bucket_key="s3://test_bucket/file", bucket_name="test_bucket")
 
     mock_client.head_object.side_effect = ClientError(
