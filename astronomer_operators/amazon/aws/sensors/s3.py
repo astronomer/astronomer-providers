322d36b4f10d6aa3d33babb10fe53cfade36afb5
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-02-12 00:13:38 +0545

    Remove poke method to do the parsing for bucket_key and bucket_name.

--- astronomer_operators/amazon/aws/sensors/s3.py
+++ astronomer_operators/amazon/aws/sensors/s3.py
@@ -1,14 +1,17 @@
 import logging
-from typing import Any, Dict
+from typing import Any, Dict, Optional, Union
+from urllib.parse import urlparse
 
-from airflow.providers.amazon.aws.sensors.s3_key import S3KeySensor
+from airflow.exceptions import AirflowException
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.sensors.base import BaseSensorOperator
 
 from astronomer_operators.amazon.aws.triggers.s3 import S3KeyTrigger
 
 log = logging.getLogger(__name__)
 
 
-class S3KeySensorAsync(S3KeySensor):
+class S3KeySensorAsync(BaseSensorOperator):
     """
     Waits for a key (a file-like instance on S3) to be present in a S3 bucket
     asynchronously. S3 being a key/value it does not support folders. The path
@@ -39,19 +42,47 @@ class S3KeySensorAsync(S3KeySensor):
     :type verify: bool or str
     """
 
+    def __init__(
+        self,
+        *,
+        bucket_key: str,
+        bucket_name: Optional[str] = None,
+        wildcard_match: bool = False,
+        aws_conn_id: str = "aws_default",
+        verify: Optional[Union[str, bool]] = None,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        self.bucket_name = bucket_name
+        self.bucket_key = bucket_key
+        self.wildcard_match = wildcard_match
+        self.aws_conn_id = aws_conn_id
+        self.verify = verify
+        self.hook: Optional[S3Hook] = None
+
+    def _resolve_bucket_and_key(self):
+        """If key is URI, parse bucket"""
+        if self.bucket_name is None:
+            self.bucket_name, self.bucket_key = S3Hook.parse_s3_url(self.bucket_key)
+        else:
+            parsed_url = urlparse(self.bucket_key)
+            if parsed_url.scheme != "" or parsed_url.netloc != "":
+                raise AirflowException("If bucket_name provided, bucket_key must be relative path, not URI.")
+
     def execute(self, context: Dict) -> Any:
-        if not self.poke(context=context):
-            self.defer(
-                timeout=self.execution_timeout,
-                trigger=S3KeyTrigger(
-                    bucket_name=self.bucket_name,
-                    bucket_key=self.bucket_key,
-                    wildcard_match=self.wildcard_match,
-                    aws_conn_id=self.aws_conn_id,
-                    verify=self.verify,
-                ),
-                method_name="execute_complete",
-            )
+        self._resolve_bucket_and_key()
+        self.defer(
+            timeout=self.execution_timeout,
+            trigger=S3KeyTrigger(
+                bucket_name=self.bucket_name,  # type: ignore
+                bucket_key=self.bucket_key,
+                wildcard_match=self.wildcard_match,
+                aws_conn_id=self.aws_conn_id,
+                verify=self.verify,
+            ),
+            method_name="execute_complete",
+        )
 
     def execute_complete(self, context: Dict, event=None):  # pylint: disable=unused-argument
         return None
