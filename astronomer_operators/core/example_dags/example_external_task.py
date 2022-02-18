f7f444c8bff62d809c0470714ed9dc71b526f8fb
Phani Kumar <94376113+phanikumv@users.noreply.github.com>
2022-02-18 19:35:32 +0530

    Move `example_dags` under respective provider folder (#58)

--- astronomer_operators/core/example_dags/example_external_task.py
+++ astronomer_operators/core/example_dags/example_external_task.py
@@ -1,7 +1,7 @@
 """
 Manually testing the async external task sensor requires a separate DAG for it to defer execution
 until the second DAG is complete.
-    1. Add this file and "external_task_wait_for_me.py" to your local airflow/dags/
+    1. Add this file and "example_external_task_wait_for_me.py" to your local airflow/dags/
     2. Once airflow is running, select "Trigger Dag w/ Config" for DAG: "test_external_task_async"
     3. Copy the timestamp and hit the Trigger button
     4. Select "Trigger DAG w/ Config" for DAG: "test_external_task_async_waits_for_me"
