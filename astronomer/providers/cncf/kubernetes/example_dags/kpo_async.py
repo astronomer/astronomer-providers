e1bcd29f1649728fdbe0e9fc128af09f1bc37316
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2022-02-21 10:00:00 -0800

    Add KPO async example dag

---
+++ astronomer/providers/cncf/kubernetes/example_dags/kpo_async.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.configuration import conf
+
+from astronomer.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperatorAsync,
+)
+
+namespace = conf.get("kubernetes", "NAMESPACE")
+
+if namespace == "default":
+    config_file = None
+    in_cluster = False
+else:
+    in_cluster = True
+    config_file = None
+
+
+with DAG(
+    dag_id="kpo_async",
+    start_date=datetime(2022, 1, 1),
+    schedule_interval="@daily",
+    tags=["k8s", "core", "async"],
+    max_active_runs=1,
+) as dag:
+    simple_async = KubernetesPodOperatorAsync(
+        task_id="simple_async",
+        namespace=namespace,
+        in_cluster=in_cluster,
+        config_file=config_file,
+        name="simple_async",
+        image="ubuntu",
+        cmds=["/bin/sh"],
+        arguments=["-c", "sleep 30"],
+    )
