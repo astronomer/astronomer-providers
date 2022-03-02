0c6f7e444cf0b1f448a064b5ba1c70aa84ee4b5e
Rajath <92459020+rajaths010494@users.noreply.github.com>
2022-03-02 19:04:31 +0530

    Implement GCSObjectWithPrefixExistenceSensor
    - Implement the GCSPrefixBlobTrigger(
    - Implement the sensor GCSObjectWithPrefixExistenceSensor

--- astronomer/providers/google/cloud/sensors/gcs.py
+++ astronomer/providers/google/cloud/sensors/gcs.py
@@ -1,11 +1,17 @@
 """This module contains Google Cloud Storage sensors."""
 
-from typing import Optional, Sequence, Union
+from typing import TYPE_CHECKING, List, Optional, Sequence, Union
 
 from airflow.exceptions import AirflowException
 from airflow.models.baseoperator import BaseOperator
 
-from astronomer.providers.google.cloud.triggers.gcs import GCSBlobTrigger
+from astronomer.providers.google.cloud.triggers.gcs import (
+    GCSBlobTrigger,
+    GCSPrefixBlobTrigger,
+)
+
+if TYPE_CHECKING:
+    from airflow.utils.context import Context
 
 
 class GCSObjectExistenceSensorAsync(BaseOperator):
@@ -55,7 +61,7 @@ class GCSObjectExistenceSensorAsync(BaseOperator):
         self.delegate_to = delegate_to
         self.impersonation_chain = impersonation_chain
 
-    def execute(self, context):
+    def execute(self, context: "Context"):
         self.defer(
             timeout=self.execution_timeout,
             trigger=GCSBlobTrigger(
@@ -68,7 +74,7 @@ class GCSObjectExistenceSensorAsync(BaseOperator):
             method_name="execute_complete",
         )
 
-    def execute_complete(self, context, event=None):  # pylint: disable=unused-argument
+    def execute_complete(self, context: "Context", event=None):  # pylint: disable=unused-argument
         """
         Callback for when the trigger fires - returns immediately.
         Relies on trigger to throw an exception, otherwise it assumes execution was
@@ -78,3 +84,86 @@ class GCSObjectExistenceSensorAsync(BaseOperator):
             raise AirflowException(event["message"])
         self.log.info("File %s was found in bucket %s.", self.object, self.bucket)
         return event["message"]
+
+
+class GCSObjectsWithPrefixExistenceSensorAsync(BaseOperator):
+    """
+    Checks for the existence of GCS objects at a given prefix, passing matches via XCom.
+
+    When files matching the given prefix are found, once match is found
+    and the matching objects will be returned from the operator and passed
+    through XCom for downstream tasks.
+
+    :param bucket: The Google Cloud Storage bucket where the object is.
+    :type bucket: str
+    :param prefix: The name of the prefix to check in the Google cloud
+        storage bucket.
+    :type prefix: str
+    :param google_cloud_conn_id: The connection ID to use when
+        connecting to Google Cloud Storage.
+    :type google_cloud_conn_id: str
+    :param delegate_to: The account to impersonate using domain-wide delegation of authority,
+        if any. For this to work, the service account making the request must have
+        domain-wide delegation enabled.
+    :type delegate_to: str
+    :param impersonation_chain: Optional service account to impersonate using short-term
+        credentials, or chained list of accounts required to get the access_token
+        of the last account in the list, which will be impersonated in the request.
+        If set as a string, the account must grant the originating account
+        the Service Account Token Creator IAM role.
+        If set as a sequence, the identities from the list must grant
+        Service Account Token Creator IAM role to the directly preceding identity, with first
+        account from the list granting this role to the originating account (templated).
+    :type impersonation_chain: Union[str, Sequence[str]]
+    """
+
+    template_fields: Sequence[str] = (
+        'bucket',
+        'prefix',
+        'impersonation_chain',
+    )
+    ui_color = '#f0eee4'
+
+    def __init__(
+        self,
+        bucket: str,
+        prefix: str,
+        google_cloud_conn_id: str = 'google_cloud_default',
+        polling_interval: float = 5.0,
+        delegate_to: Optional[str] = None,
+        impersonation_chain: Optional[Union[str, Sequence[str]]] = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.bucket = bucket
+        self.prefix = prefix
+        self.google_cloud_conn_id = google_cloud_conn_id
+        self.polling_interval = polling_interval
+        self.delegate_to = delegate_to
+        self.impersonation_chain = impersonation_chain
+
+    def execute(self, context: "Context"):
+        self.defer(
+            timeout=self.execution_timeout,
+            trigger=GCSPrefixBlobTrigger(
+                bucket=self.bucket,
+                prefix=self.prefix,
+                polling_period_seconds=self.polling_interval,
+                google_cloud_conn_id=self.google_cloud_conn_id,
+                hook_params=dict(delegate_to=self.delegate_to, impersonation_chain=self.impersonation_chain),
+            ),
+            method_name="execute_complete",
+        )
+
+    def execute_complete(
+        self, context: "Context", event=None
+    ) -> List[str]:  # pylint: disable=unused-argument
+        """
+        Callback for when the trigger fires - returns immediately.
+        Relies on trigger to throw an exception, otherwise it assumes execution was
+        successful.
+        """
+        self.log.info('Sensor checks existence of objects: %s, %s', self.bucket, self.prefix)
+        if event["status"] == "success":
+            return event["matches"]
+        raise AirflowException(event["message"])
--- astronomer/providers/google/cloud/triggers/gcs.py
+++ astronomer/providers/google/cloud/triggers/gcs.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, List, Tuple
 
 from aiohttp import ClientSession as Session
 from airflow.triggers.base import BaseTrigger, TriggerEvent
@@ -87,3 +87,77 @@ class GCSBlobTrigger(BaseTrigger):
             else:
                 res = "pending"
             return res
+
+
+class GCSPrefixBlobTrigger(GCSBlobTrigger):
+    """
+    A trigger that fires and it finds the requested file or folder present in the given bucket.
+
+    :param bucket: the bucket in the google cloud storage where the objects are residing.
+    :param object_name: the file or folder present in the bucket
+    :param google_cloud_conn_id: reference to the Google Connection
+    :param polling_period_seconds: polling period in seconds to check for file/folder
+    """
+
+    def __init__(
+        self,
+        bucket: str,
+        prefix: str,
+        polling_period_seconds: float,
+        google_cloud_conn_id: str,
+        hook_params: dict,
+    ):
+        super().__init__()
+        self.bucket = bucket
+        self.prefix = prefix
+        self.polling_period_seconds = polling_period_seconds
+        self.google_cloud_conn_id: str = google_cloud_conn_id
+        self.hook_params = hook_params
+
+    def serialize(self) -> Tuple[str, Dict[str, Any]]:
+        """
+        Serializes GCSBlobTrigger arguments and classpath.
+        """
+        return (
+            "astronomer.providers.google.cloud.triggers.gcs.GCSPrefixBlobTrigger",
+            {
+                "bucket": self.bucket,
+                "prefix": self.prefix,
+                "polling_period_seconds": self.polling_period_seconds,
+                "google_cloud_conn_id": self.google_cloud_conn_id,
+                "hook_params": self.hook_params,
+            },
+        )
+
+    async def run(self):
+        """
+        Simple loop until the relevant file/folder is found.
+        """
+        try:
+            hook = self._get_async_hook()
+            while True:
+                res = await self._object_with_prefix_exists(
+                    hook=hook, bucket_name=self.bucket, object_name=self.prefix
+                )
+                if len(res) > 0:
+                    yield TriggerEvent({"status": "success", "message": "Successfully completed", "matches": res})
+                    return
+                await asyncio.sleep(self.polling_period_seconds)
+        except Exception as e:
+            yield TriggerEvent({"status": "error", "message": str(e)})
+            return
+
+    async def _object_with_prefix_exists(
+        self, hook: GCSHookAsync, bucket_name: str, prefix: str
+    ) -> List[str]:
+        """
+        Checks for the existence of a file in Google Cloud Storage.
+        :param bucket_name: The Google Cloud Storage bucket where the object is.
+        :param object_name: The name of the blob_name to check in the Google cloud
+            storage bucket.
+        """
+        async with Session() as s:
+            client = await hook.get_storage_client(s)
+            bucket = client.get_bucket(bucket_name)
+            object_response = await bucket.list_blobs(prefix=prefix)
+            return object_response
