97a3669c87b574335b08db3a87f3a482c6a71aca
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-03-02 21:32:53 +0545

    Add example DAG for S3KeySizeSensorAsync

--- astronomer/providers/amazon/aws/example_dags/example_s3.py
+++ astronomer/providers/amazon/aws/example_dags/example_s3.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime, timedelta
 
-from airflow import DAG
+from airflow.models.dag import DAG
 from airflow.providers.amazon.aws.operators.s3 import (
     S3CreateBucketOperator,
     S3DeleteBucketOperator,
