1e7820512a12b8fc0a6a8f2878abd1b22b346260
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-03-03 15:14:30 +0545

    Add tests for S3KeySizeSensorAsync

--- tests/amazon/aws/sensors/test_s3_sensors.py
+++ tests/amazon/aws/sensors/test_s3_sensors.py
@@ -8,8 +8,11 @@ from airflow.models.variable import Variable
 from airflow.utils import timezone
 from parameterized import parameterized
 
-from astronomer.providers.amazon.aws.sensors.s3 import S3KeySensorAsync
-from astronomer.providers.amazon.aws.triggers.s3 import S3KeyTrigger
+from astronomer.providers.amazon.aws.sensors.s3 import (
+    S3KeySensorAsync,
+    S3KeySizeSensorAsync,
+)
+from astronomer.providers.amazon.aws.triggers.s3 import S3KeySizeTrigger, S3KeyTrigger
 
 
 @pytest.fixture
@@ -178,3 +181,136 @@ class TestS3KeySensorAsync(unittest.TestCase):
             sensor.execute(context)
 
         assert isinstance(exc.value.trigger, S3KeyTrigger), "Trigger is not a S3KeyTrigger"
+
+
+class TestS3KeySizeSensorAsync(unittest.TestCase):
+    def test_bucket_name_none_and_bucket_key_as_relative_path(self):
+        """
+        Test if exception is raised when bucket_name is None
+        and bucket_key is provided as relative path rather than s3:// url.
+        :return:
+        """
+        op = S3KeySizeSensorAsync(task_id="s3_key_size_sensor", bucket_key="file_in_bucket")
+        with pytest.raises(AirflowException):
+            op._resolve_bucket_and_key()
+
+    def test_bucket_name_provided_and_bucket_key_is_s3_url(self):
+        """
+        Test if exception is raised when bucket_name is provided
+        while bucket_key is provided as a full s3:// url.
+        :return:
+        """
+        op = S3KeySizeSensorAsync(
+            task_id="s3_key_size_sensor", bucket_key="s3://test_bucket/file", bucket_name="test_bucket"
+        )
+        with pytest.raises(AirflowException):
+            op._resolve_bucket_and_key()
+
+    @parameterized.expand([["s3://bucket/key", None, "key", "bucket"], ["key", "bucket", "key", "bucket"]])
+    @mock.patch("airflow.providers.amazon.aws.sensors.s3.S3Hook.check_for_key")
+    def test_parse_bucket_key(self, key, bucket, parsed_key, parsed_bucket, mock_check):
+
+        mock_check.return_value = False
+
+        op = S3KeySizeSensorAsync(
+            task_id="s3_key_size_sensor_async",
+            bucket_key=key,
+            bucket_name=bucket,
+        )
+
+        op._resolve_bucket_and_key()
+
+        assert op.bucket_key == parsed_key
+        assert op.bucket_name == parsed_bucket
+
+    @parameterized.expand(
+        [
+            ["s3://bucket/key", None],
+            ["key", "bucket"],
+        ]
+    )
+    @mock.patch("airflow.providers.amazon.aws.sensors.s3.S3Hook")
+    def test_s3_key_sensor_async(self, key, bucket, mock_hook):
+        """
+        Asserts that a task is deferred and an S3KeyTrigger will be fired
+        when the S3KeySizeSensorAsync is executed.
+        """
+        mock_hook.check_for_key.return_value = False
+
+        sensor = S3KeySizeSensorAsync(
+            task_id="s3_key_size_sensor_async",
+            bucket_key=key,
+            bucket_name=bucket,
+        )
+
+        with pytest.raises(TaskDeferred) as exc:
+            sensor.execute(context)
+
+        assert isinstance(exc.value.trigger, S3KeySizeTrigger), "Trigger is not a S3KeySizeTrigger"
+
+    @parameterized.expand(
+        [
+            ["s3://bucket/key", None],
+            ["key", "bucket"],
+        ]
+    )
+    @mock.patch("airflow.providers.amazon.aws.sensors.s3.S3Hook")
+    def test_s3_key_sensor_execute_complete(self, key, bucket, mock_hook):
+        """
+        Asserts that a task is deferred and an S3KeyTrigger will be fired
+        when the S3KeySizeSensorAsync is executed.
+        """
+        mock_hook.check_for_key.return_value = False
+
+        sensor = S3KeySizeSensorAsync(
+            task_id="s3_key_size_sensor_async",
+            bucket_key=key,
+            bucket_name=bucket,
+        )
+        assert sensor.execute_complete(context) is None
+
+    @parameterized.expand(
+        [
+            ["s3://bucket/key", None],
+            ["key", "bucket"],
+        ]
+    )
+    @mock.patch("airflow.providers.amazon.aws.sensors.s3.S3Hook")
+    @mock.patch.object(S3KeySizeSensorAsync, "defer")
+    @mock.patch("astronomer.providers.amazon.aws.sensors.s3.S3KeySizeTrigger")
+    def test_s3_key_sensor_async_with_mock_defer(self, key, bucket, mock_trigger, mock_defer, mock_hook):
+        """
+        Asserts that a task is deferred and an S3KeyTrigger will be fired
+        when the S3KeySizeSensorAsync is executed.
+        """
+        mock_hook.check_for_key.return_value = False
+
+        sensor = S3KeySizeSensorAsync(
+            task_id="s3_key_size_sensor_async",
+            bucket_key=key,
+            bucket_name=bucket,
+        )
+
+        sensor.execute(context)
+
+        mock_defer.assert_called()
+        mock_defer.assert_called_once_with(
+            timeout=None, trigger=mock_trigger.return_value, method_name="execute_complete"
+        )
+
+    @mock.patch("airflow.providers.amazon.aws.sensors.s3.S3Hook")
+    def test_s3_key_sensor_with_wildcard_async(self, mock_hook):
+        """
+        Asserts that a task with wildcard=True is deferred and an S3KeyTrigger will be fired
+        when the S3KeySizeSensorAsync is executed.
+        """
+        mock_hook.check_for_key.return_value = False
+
+        sensor = S3KeySizeSensorAsync(
+            task_id="s3_key_size_sensor_async", bucket_key="s3://test_bucket/file", wildcard_match=True
+        )
+
+        with pytest.raises(TaskDeferred) as exc:
+            sensor.execute(context)
+
+        assert isinstance(exc.value.trigger, S3KeySizeTrigger), "Trigger is not a S3KeyTrigger"
--- tests/amazon/aws/triggers/test_s3_triggers.py
+++ tests/amazon/aws/triggers/test_s3_triggers.py
@@ -3,7 +3,7 @@ from unittest import mock
 
 import pytest
 
-from astronomer.providers.amazon.aws.triggers.s3 import S3KeyTrigger
+from astronomer.providers.amazon.aws.triggers.s3 import S3KeySizeTrigger, S3KeyTrigger
 
 
 def test_s3_key_trigger_serialization():
@@ -38,3 +38,51 @@ async def test_s3_key_trigger_run(mock_client):
 
         assert task.done() is True
         asyncio.get_event_loop().stop()
+
+
+def test_s3_key_size_trigger_serialization():
+    """
+    Asserts that the TaskStateTrigger correctly serializes its arguments
+    and classpath.
+    """
+    trigger = S3KeySizeTrigger(
+        bucket_key="s3://test_bucket/file", bucket_name="test_bucket", wildcard_match=True
+    )
+    classpath, kwargs = trigger.serialize()
+    assert classpath == "astronomer.providers.amazon.aws.triggers.s3.S3KeySizeTrigger"
+    assert kwargs == {
+        "bucket_name": "test_bucket",
+        "bucket_key": "s3://test_bucket/file",
+        "wildcard_match": True,
+        "aws_conn_id": "aws_default",
+        "hook_params": {},
+        "check_fn_user": None,
+    }
+
+
+@pytest.mark.asyncio
+@mock.patch("astronomer.providers.amazon.aws.triggers.s3.S3HookAsync.get_client_async")
+async def test_s3_key_size_trigger_run(mock_client):
+    """
+    Test if the task is run is in triggerr successfully.
+    :return:
+    """
+    mock_client.return_value.check_key.return_value = True
+    trigger = S3KeySizeTrigger(bucket_key="s3://test_bucket/file", bucket_name="test_bucket")
+    with mock_client:
+        task = asyncio.create_task(trigger.run().__anext__())
+        await asyncio.sleep(0.5)
+
+        assert task.done() is True
+        asyncio.get_event_loop().stop()
+
+
+def test_s3_key_size_check_fn_trigger_run():
+    """
+    Test if the _check_fn returns True.
+    :return:
+    """
+    trigger = S3KeySizeTrigger(bucket_key="s3://test_bucket/file", bucket_name="test_bucket")
+
+    response = trigger._check_fn(data=[])
+    assert response is True
