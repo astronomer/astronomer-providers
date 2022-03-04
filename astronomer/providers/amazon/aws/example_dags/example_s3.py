3d9af809ca7095a0ddcded6f686ca64988380ace
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-03-04 21:30:17 +0545

    Add example DAG for S3KeysUnchangedSensorAsync along with use of environment variable

--- astronomer/providers/amazon/aws/example_dags/example_s3.py
+++ astronomer/providers/amazon/aws/example_dags/example_s3.py
@@ -1,16 +1,27 @@
+import os
 from datetime import datetime, timedelta
 
-from airflow.models.dag import DAG
+from airflow import DAG
 
 from astronomer.providers.amazon.aws.sensors.s3 import (
     S3KeySensorAsync,
     S3KeySizeSensorAsync,
+    S3KeysUnchangedSensorAsync,
 )
 
 default_args = {
     "retry": 5,
     "retry_delay": timedelta(minutes=1),
 }
+
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "astronomer-airflow-providers")
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "test-team-providers")
+S3_BUCKET_KEY = os.environ.get("S3_BUCKET_KEY", "sample_key.txt")
+S3_BUCKET_WILDCARD_KEY = os.environ.get("S3_BUCKET_WILDCARD_KEY", "test*")
+WILDCARD_KEY_FALSE = os.environ.get("WILDCARD_KEY_FALSE", False)
+WILDCARD_KEY_TRUE = os.environ.get("WILDCARD_KEY_TRUE", True)
+PREFIX = os.environ.get("PREFIX", "test")
+INACTIVITY_PERIOD = os.environ.get("INACTIVITY_PERIOD", 5)
 
 with DAG(
     dag_id="example_s3_key_sensor",
@@ -23,31 +34,43 @@ with DAG(
 
     waiting_for_s3_key = S3KeySensorAsync(
         task_id="waiting_for_s3_key",
-        bucket_key="sample_key.txt",
-        wildcard_match=False,
-        bucket_name="sample-bucket",
+        bucket_key=S3_BUCKET_KEY,
+        wildcard_match=WILDCARD_KEY_FALSE,
+        bucket_name=S3_BUCKET_NAME,
     )
 
     check_if_wildcard_exists = S3KeySensorAsync(
         task_id="check_if_wildcard_exists",
-        bucket_key="test*",
-        wildcard_match=True,
-        bucket_name="sample-bucket",
+        bucket_key=S3_BUCKET_WILDCARD_KEY,
+        wildcard_match=WILDCARD_KEY_TRUE,
+        bucket_name=S3_BUCKET_NAME,
     )
 
     check_if_key_with_size_without_wildcard = S3KeySizeSensorAsync(
         task_id="check_if_key_with_size_without_wildcard",
-        bucket_key="sample_key.txt",
-        wildcard_match=False,
-        bucket_name="test-team-providers-ankit",
+        bucket_key=S3_BUCKET_KEY,
+        wildcard_match=WILDCARD_KEY_FALSE,
+        bucket_name=S3_BUCKET_NAME,
     )
 
     check_if_key_with_size_with_wildcard = S3KeySizeSensorAsync(
         task_id="check_if_key_with_size_with_wildcard",
-        bucket_key="sample*",
-        wildcard_match=True,
-        bucket_name="test-team-providers-ankit",
+        bucket_key=S3_BUCKET_WILDCARD_KEY,
+        wildcard_match=WILDCARD_KEY_TRUE,
+        bucket_name=S3_BUCKET_NAME,
     )
 
-    waiting_for_s3_key >> check_if_wildcard_exists >> check_if_key_with_size_without_wildcard
-    check_if_key_with_size_without_wildcard >> check_if_key_with_size_with_wildcard
+    check_s3_key_unchanged_sensor = S3KeysUnchangedSensorAsync(
+        task_id="check_s3_key_unchanged_sensor",
+        bucket_name=S3_BUCKET_NAME,
+        prefix=PREFIX,
+        inactivity_period=INACTIVITY_PERIOD,
+    )
+
+(
+    waiting_for_s3_key
+    >> check_if_wildcard_exists
+    >> check_if_key_with_size_without_wildcard
+    >> check_if_key_with_size_with_wildcard
+    >> check_s3_key_unchanged_sensor
+)
