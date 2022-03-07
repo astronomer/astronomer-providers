d50b4d73f0dc3cd53193b9bfdc6abe8f23204bb9
bharanidharan14 <bharanidharan.manimuthu@astronomer.io>
2022-03-08 00:54:10 +0530

    Redshift cluster test coverage fix

--- tests/amazon/aws/hooks/test_redshift_cluster.py
+++ tests/amazon/aws/hooks/test_redshift_cluster.py
@@ -1,6 +1,8 @@
+import asyncio
 from unittest import mock
 
 import pytest
+from botocore.exceptions import ClientError
 
 from astronomer.providers.amazon.aws.hooks.redshift_cluster import RedshiftHookAsync
 
@@ -29,33 +31,48 @@ from astronomer.providers.amazon.aws.hooks.redshift_cluster import RedshiftHookA
             {"status": "success", "cluster_state": "resuming"},
             {"Clusters": [{"ClusterStatus": "resuming"}]},
         ),
-        ("astro-redshift-cluster-1", None, {"Clusters": []}),
-        (
-            "astro-redshift-cluster-1",
-            {
-                "status": "error",
-                "message": "ClusterNotFoundFault('An error occurred "
-                "(ClusterNotFound) when calling the "
-                "DescribeClusters "
-                "operation: Cluster test111 not found.",
-            },
-            "ClusterNotFoundFault('An error occurred "
-            "(ClusterNotFound) when calling the "
-            "DescribeClusters "
-            "operation: Cluster test111 not found.",
-        ),
+        ("astro-redshift-cluster-1", {"status": "success", "cluster_state": None}, {"Clusters": []}),
     ],
 )
-@mock.patch("astronomer.providers.amazon.aws.hooks.redshift_cluster.RedshiftHookAsync.cluster_status")
 @mock.patch("astronomer.providers.amazon.aws.hooks.redshift_cluster.RedshiftHookAsync.get_client_async")
-async def test_redshift_cluster_status(
-    mock_client, mock_cluster_status, mock_cluster_identifier, cluster_state, expected_result
-):
-    hook = RedshiftHookAsync(aws_conn_id="test_aws_connection_id")
-    mock_client.return_value.describe_clusters.return_value = expected_result
-    mock_cluster_status.return_value = cluster_state
+async def test_redshift_cluster_status(mock_client, mock_cluster_identifier, cluster_state, expected_result):
+    mock_client.return_value.__aenter__.return_value.describe_clusters.return_value = expected_result
+    hook = RedshiftHookAsync(
+        aws_conn_id="test_aws_connection_id", client_type="redshift", resource_type="redshift"
+    )
     result = await hook.cluster_status(cluster_identifier=mock_cluster_identifier)
+    print(result)
     assert result == cluster_state
+
+
+@pytest.mark.asyncio
+@mock.patch("astronomer.providers.amazon.aws.hooks.redshift_cluster.RedshiftHookAsync.get_client_async")
+async def test_redshift_cluster_status_exception(mock_client):
+    mock_client.return_value.__aenter__.return_value.describe_clusters.side_effect = ClientError(
+        {
+            "Error": {
+                "Code": "SomeServiceException",
+                "Message": "Details/context around the exception or error",
+            },
+            "ResponseMetadata": {
+                "RequestId": "1234567890ABCDEF",
+                "HostId": "host ID data will appear here as a hash",
+                "HTTPStatusCode": 500,
+                "HTTPHeaders": {"header metadata key/values will appear here"},
+                "RetryAttempts": 0,
+            },
+        },
+        operation_name="redshift",
+    )
+    hook = RedshiftHookAsync(
+        aws_conn_id="test_aws_connection_id", client_type="redshift", resource_type="redshift"
+    )
+    result = await hook.cluster_status(cluster_identifier="astro-redshift-cluster-1")
+    assert result == {
+        "status": "error",
+        "message": "An error occurred (SomeServiceException) when calling the "
+        "redshift operation: Details/context around the exception or error",
+    }
 
 
 @pytest.mark.asyncio
@@ -124,3 +141,43 @@ async def test_resume_cluster(
     mock_cluster_status.return_value = cluster_state
     result = await hook.resume_cluster(cluster_identifier=mock_cluster_identifier)
     assert result == cluster_state
+
+
+@pytest.mark.asyncio
+@mock.patch("astronomer.providers.amazon.aws.hooks.redshift_cluster.RedshiftHookAsync.get_client_async")
+@mock.patch("astronomer.providers.amazon.aws.hooks.redshift_cluster.RedshiftHookAsync.cluster_status")
+async def test_get_cluster_status(cluster_status, mock_client):
+    flag = asyncio.Event()
+    cluster_status.return_value = {"status": "success", "cluster_state": "available"}
+    hook = RedshiftHookAsync(aws_conn_id="test_aws_connection_id")
+    result = await hook.get_cluster_status("test-identifier", "available", flag)
+    assert result == {"status": "success", "cluster_state": "available"}
+
+
+@pytest.mark.asyncio
+@mock.patch("astronomer.providers.amazon.aws.hooks.redshift_cluster.RedshiftHookAsync.cluster_status")
+async def test_get_cluster_status_exception(cluster_status):
+    flag = asyncio.Event()
+    cluster_status.side_effect = ClientError(
+        {
+            "Error": {
+                "Code": "SomeServiceException",
+                "Message": "Details/context around the exception or error",
+            },
+            "ResponseMetadata": {
+                "RequestId": "1234567890ABCDEF",
+                "HostId": "host ID data will appear here as a hash",
+                "HTTPStatusCode": 500,
+                "HTTPHeaders": {"header metadata key/values will appear here"},
+                "RetryAttempts": 0,
+            },
+        },
+        operation_name="redshift",
+    )
+    hook = RedshiftHookAsync(aws_conn_id="test_aws_connection_id")
+    result = await hook.get_cluster_status("test-identifier", "available", flag)
+    assert result == {
+        "status": "error",
+        "message": "An error occurred (SomeServiceException) when calling the "
+        "redshift operation: Details/context around the exception or error",
+    }
