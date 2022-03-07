236024be56d984bdcb8c54a328597f43abbb3e14
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-03-08 00:45:22 +0545

    Make DAG self sufficient for s3 async operators

--- astronomer/providers/amazon/aws/example_dags/example_s3.py
+++ astronomer/providers/amazon/aws/example_dags/example_s3.py
@@ -2,6 +2,13 @@ import os
 from datetime import datetime, timedelta
 
 from airflow import DAG
+from airflow.providers.amazon.aws.operators.s3 import (
+    S3CreateBucketOperator,
+    S3DeleteBucketOperator,
+)
+from airflow.providers.amazon.aws.transfers.local_to_s3 import (
+    LocalFilesystemToS3Operator,
+)
 
 from astronomer.providers.amazon.aws.sensors.s3 import (
     S3KeySensorAsync,
@@ -15,13 +22,15 @@ default_args = {
 }
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "astronomer-airflow-providers")
-S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "test-team-providers")
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "test-bucket")
 S3_BUCKET_KEY = os.environ.get("S3_BUCKET_KEY", "sample_key.txt")
-S3_BUCKET_WILDCARD_KEY = os.environ.get("S3_BUCKET_WILDCARD_KEY", "test*")
+S3_BUCKET_WILDCARD_KEY = os.environ.get("S3_BUCKET_WILDCARD_KEY", "sam*")
 WILDCARD_KEY_FALSE = os.environ.get("WILDCARD_KEY_FALSE", False)
 WILDCARD_KEY_TRUE = os.environ.get("WILDCARD_KEY_TRUE", True)
 PREFIX = os.environ.get("PREFIX", "test")
 INACTIVITY_PERIOD = os.environ.get("INACTIVITY_PERIOD", 5)
+REGION_NAME = os.environ.get("REGION_NAME", "us-east-2")
+LOCAL_FILE_PATH = os.environ.get("LOCAL_FILE_PATH", "/usr/local/airflow/sample_key.txt")
 
 with DAG(
     dag_id="example_s3_key_sensor",
@@ -31,6 +40,16 @@ with DAG(
     default_args=default_args,
     tags=["async"],
 ) as dag:
+    create_bucket = S3CreateBucketOperator(
+        task_id='create_bucket', region_name=REGION_NAME, bucket_name=S3_BUCKET_NAME
+    )
+
+    create_local_to_s3_job = LocalFilesystemToS3Operator(
+        task_id="create_local_to_s3_job",
+        filename=LOCAL_FILE_PATH,
+        dest_key=S3_BUCKET_KEY,
+        dest_bucket=S3_BUCKET_NAME,
+    )
 
     waiting_for_s3_key = S3KeySensorAsync(
         task_id="waiting_for_s3_key",
@@ -67,10 +86,17 @@ with DAG(
         inactivity_period=INACTIVITY_PERIOD,
     )
 
+    delete_bucket = S3DeleteBucketOperator(
+        task_id='delete_bucket', force_delete=True, bucket_name=S3_BUCKET_NAME
+    )
+
 (
-    waiting_for_s3_key
+    create_bucket
+    >> create_local_to_s3_job
+    >> waiting_for_s3_key
     >> check_if_wildcard_exists
     >> check_if_key_with_size_without_wildcard
     >> check_if_key_with_size_with_wildcard
     >> check_s3_key_unchanged_sensor
+    >> delete_bucket
 )
