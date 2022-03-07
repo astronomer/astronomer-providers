8fcb589e89f627cf908b89c7254298c2ece9a09f
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-03-08 00:05:28 +0545

    Add missing test in test_s3_triggers to improve coverage

--- tests/amazon/aws/triggers/test_s3_triggers.py
+++ tests/amazon/aws/triggers/test_s3_triggers.py
@@ -2,6 +2,7 @@ import asyncio
 from unittest import mock
 
 import pytest
+from airflow.triggers.base import TriggerEvent
 
 from astronomer.providers.amazon.aws.triggers.s3 import (
     S3KeySizeTrigger,
@@ -81,6 +82,42 @@ async def test_s3_key_size_trigger_run(mock_client):
         asyncio.get_event_loop().stop()
 
 
+@pytest.mark.asyncio
+@mock.patch("astronomer.providers.amazon.aws.triggers.s3.S3HookAsync.get_client_async")
+@mock.patch("astronomer.providers.amazon.aws.triggers.s3.S3HookAsync.get_files")
+async def test_s3_key_size_trigger_run_check_fn_user_success(mock_get_files, mock_client):
+    """
+    Test if the task is run is in triggerr with check_fn_user defined by user.
+    """
+    mock_get_files.return_value = True
+
+    def dummy_check_fn(list_obj):
+        return True
+
+    mock_client.return_value.check_key.return_value = True
+    trigger = S3KeySizeTrigger(
+        bucket_key="s3://test_bucket/file", bucket_name="test_bucket", check_fn=dummy_check_fn
+    )
+    task = [i async for i in trigger.run()]
+    assert len(task) == 1
+    assert TriggerEvent({"status": "success"}) in task
+
+
+@pytest.mark.asyncio
+@mock.patch("astronomer.providers.amazon.aws.triggers.s3.S3HookAsync.get_client_async")
+@mock.patch("astronomer.providers.amazon.aws.triggers.s3.S3HookAsync.get_files")
+async def test_s3_key_size_trigger_run_check_fn_success(mock_get_files, mock_client):
+    """
+    Test if the task is run is in triggerr with check_fn.
+    """
+    mock_get_files.return_value = ["test"]
+    mock_client.return_value.check_key.return_value = True
+    trigger = S3KeySizeTrigger(bucket_key="s3://test_bucket/file", bucket_name="test_bucket")
+    task = [i async for i in trigger.run()]
+    assert len(task) == 1
+    assert TriggerEvent({"status": "success"}) in task
+
+
 def test_s3_key_size_check_fn_trigger_run():
     """
     Test if the _check_fn returns True.
@@ -135,3 +172,26 @@ async def test_s3_keys_unchanged_trigger_run(mock_client):
 
         assert task.done() is True
         asyncio.get_event_loop().stop()
+
+
+def test_s3_keys_unchanged_trigger_raise_value_error():
+    """
+    Test if the S3KeysUnchangedTrigger raises Value error for negative inactivity_period.
+    """
+    with pytest.raises(ValueError):
+        S3KeysUnchangedTrigger(bucket_name="test_bucket", prefix="test", inactivity_period=-100)
+
+
+@pytest.mark.asyncio
+@mock.patch("astronomer.providers.amazon.aws.triggers.s3.S3HookAsync.get_client_async")
+@mock.patch("astronomer.providers.amazon.aws.triggers.s3.S3HookAsync.is_keys_unchanged")
+async def test_s3_keys_unchanged_trigger_run_success(mock_is_keys_unchanged, mock_client):
+    """
+    Test if the task is run is in triggerr successfully.
+    :return:
+    """
+    mock_is_keys_unchanged.return_value = {"status": "success"}
+    trigger = S3KeysUnchangedTrigger(bucket_name="test_bucket", prefix="test")
+    task = [i async for i in trigger.run()]
+    assert len(task) == 1
+    assert TriggerEvent({"status": "success"}) in task
