a0b8c632c35b8472a278eac1087a6220b29e95c9
Kaxil Naik <kaxilnaik@gmail.com>
2022-03-11 02:19:03 +0000

    Split the dependencies into multiple extras (#113)
    This will allow users to just install dependencies of a single provider. For example if a user wants to just use K8s async operator, they shouldn't need to install GCP, AWS, Databricks or Snowflake dependencies
    
    closes https://github.com/astronomer/astronomer-providers/issues/101

---
+++ .circleci/scripts/pre_commit_setup_cfg_all_extra.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Pre-commit hook to sync an "all" extra in setup.cfg.
+It will contain all the dependencies apart from tests and mypy.
+"""
+import configparser
+from pathlib import Path
+
+repo_dir = Path(__file__).parent.parent.parent
+
+config = configparser.ConfigParser(strict=False)
+config.read(repo_dir / "setup.cfg")
+
+all_extra = []
+extra_to_exclude = {"tests", "mypy", "all"}
+for k in config["options.extras_require"].keys():
+    if k in extra_to_exclude:
+        continue
+    reqs = config["options.extras_require"][k].split()
+    all_extra.extend(reqs)
+
+expected_all_extra = set(all_extra)
+found_all_extra = set(config["options.extras_require"].get("all", "").split())
+if not found_all_extra:
+    raise SystemExit("Missing 'all' extra in setup.cfg")
+
+diff_extras = expected_all_extra - found_all_extra
+if diff_extras:
+    diff_extras_str = "\n \t" + "\n \t".join(sorted(diff_extras))
+    raise SystemExit(f"'all' extra in setup.cfg is missing some dependencies:\n {diff_extras_str}")
