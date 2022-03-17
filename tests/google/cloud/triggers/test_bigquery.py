67001a27ebb78f3b38743e4f244442a25a296b6d
bharanidharan14 <bharanidharan.manimuthu@astronomer.io>
2022-03-17 14:38:17 +0530

    Update test_bigquery.py
    - Fixed connection id

--- tests/google/cloud/triggers/test_bigquery.py
+++ tests/google/cloud/triggers/test_bigquery.py
@@ -781,7 +781,7 @@ def test_big_query_table_existence_trigger_serialization():
         "dataset_id": TEST_DATASET_ID,
         "project_id": TEST_GCP_PROJECT_ID,
         "table_id": TEST_TABLE_ID,
-        "google_cloud_conn_id": TEST_GCP_CONN_ID,
+        "gcp_conn_id": TEST_GCP_CONN_ID,
         "poll_interval": POLLING_PERIOD_SECONDS,
         "hook_params": TEST_HOOK_PARAMS,
     }
