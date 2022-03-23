3ac03e83f10bf9ef2acdac0e6c2613abf5b0d546
Phani Kumar <94376113+phanikumv@users.noreply.github.com>
2022-03-23 22:41:02 +0530

    Bump version to `1.2.0.dev1` (#152)
    Since 1.1.0 is released, we should bump this version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = "Astronomer Providers"
 author = "Astronomer Inc."
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.0"
+release = "1.2.0.dev1"
 
 
 # -- General configuration ---------------------------------------------------
