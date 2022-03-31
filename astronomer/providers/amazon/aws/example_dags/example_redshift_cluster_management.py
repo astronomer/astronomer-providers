80210db7bb5115e0895d8886a4daa4e7905ec7ce
pankajastro <98807258+pankajastro@users.noreply.github.com>
2022-03-31 12:19:51 +0530

    Automate redshift cluster creation (#166)
    * Add BashOperator for redshift create and delete cluster operation

--- astronomer/providers/amazon/aws/example_dags/example_redshift_cluster_management.py
+++ astronomer/providers/amazon/aws/example_dags/example_redshift_cluster_management.py
@@ -1,8 +1,14 @@
+import logging
 import os
+import time
 from datetime import datetime, timedelta
 
+import boto3
 from airflow import DAG
+from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+from botocore.exceptions import ClientError
 
 from astronomer.providers.amazon.aws.operators.redshift_cluster import (
     RedshiftPauseClusterOperatorAsync,
@@ -12,12 +18,135 @@ from astronomer.providers.amazon.aws.sensors.redshift_cluster import (
     RedshiftClusterSensorAsync,
 )
 
-REDSHIFT_CLUSTER_IDENTIFIER = os.environ.get("REDSHIFT_CLUSTER_IDENTIFIER", "astro-redshift-cluster-1")
+REDSHIFT_CLUSTER_IDENTIFIER = os.environ.get("REDSHIFT_CLUSTER_IDENTIFIER", "astro-providers-cluster")
+REDSHIFT_CLUSTER_MASTER_USER = os.environ.get("REDSHIFT_CLUSTER_MASTER_USER", "adminuser")
+REDSHIFT_CLUSTER_MASTER_PASSWORD = os.environ.get("REDSHIFT_CLUSTER_MASTER_PASSWORD", "********")
+REDSHIFT_CLUSTER_DB_NAME = os.environ.get("REDSHIFT_CLUSTER_DB_NAME", "astro_dev")
+REDSHIFT_CLUSTER_TYPE = os.environ.get("REDSHIFT_CLUSTER_TYPE", "single-node")
+REDSHIFT_CLUSTER_NODE_TYPE = os.environ.get("REDSHIFT_CLUSTER_NODE_TYPE", "dc2.large")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "**********")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "***********")
+AWS_DEFAULT_REGION = os.environ.get("AWS_DEFAULT_REGION", "us-east-1")
 AWS_CONN_ID = os.environ.get("ASTRO_AWS_CONN_ID", "aws_default")
 
 default_args = {
     "execution_timeout": timedelta(minutes=30),
 }
+
+
+def get_cluster_status() -> str:
+    """Get the status of aws redshift cluster"""
+    client = boto3.client("redshift")
+
+    response = client.describe_clusters(
+        ClusterIdentifier=REDSHIFT_CLUSTER_IDENTIFIER,
+    )
+    logging.info("%s", response)
+    cluster = response.get("Clusters")[0]
+    cluster_status: str = cluster.get("ClusterStatus")
+    return cluster_status
+
+
+def get_snapshot_status() -> str:
+    """Get the status of aws redshift cluster snapshot"""
+    client = boto3.client("redshift")
+
+    response = client.describe_cluster_snapshots(
+        SnapshotIdentifier=f"{REDSHIFT_CLUSTER_IDENTIFIER}-snapshot",
+    )
+    logging.info("%s", response)
+    snapshot = response.get("Snapshots")[0]
+    snapshot_status: str = snapshot.get("Status")
+    return snapshot_status
+
+
+def create_redshift_cluster() -> None:
+    """Create aws redshift cluster and wait until it available"""
+    client = boto3.client("redshift")
+
+    client.create_cluster(
+        DBName=REDSHIFT_CLUSTER_DB_NAME,
+        ClusterIdentifier=REDSHIFT_CLUSTER_IDENTIFIER,
+        ClusterType=REDSHIFT_CLUSTER_TYPE,
+        NodeType=REDSHIFT_CLUSTER_NODE_TYPE,
+        MasterUsername=REDSHIFT_CLUSTER_MASTER_USER,
+        MasterUserPassword=REDSHIFT_CLUSTER_MASTER_PASSWORD,
+        Tags=[
+            {"Key": "Purpose", "Value": "ProviderTest"},
+        ],
+    )
+
+    while True:
+        if get_cluster_status() == "available":
+            break
+        time.sleep(30)
+
+
+def create_redshift_cluster_snapshot() -> None:
+    """Create aws redshift cluster snapshot and wait until it available"""
+    client = boto3.client("redshift")
+    client.create_cluster_snapshot(
+        SnapshotIdentifier=f"{REDSHIFT_CLUSTER_IDENTIFIER}-snapshot",
+        ClusterIdentifier=REDSHIFT_CLUSTER_IDENTIFIER,
+        Tags=[
+            {"Key": "Purpose", "Value": "ProviderTest"},
+        ],
+    )
+
+    while True:
+        if get_snapshot_status() == "available":
+            break
+        time.sleep(30)
+    # Sometime cluster take few seconds to goes to modifying state again
+    time.sleep(60)
+    while True:
+        if get_cluster_status() == "available":
+            break
+        time.sleep(30)
+
+
+def delete_redshift_cluster_snapshot() -> None:
+    """Delete a aws redshift cluster and wait until cluster is available"""
+    client = boto3.client("redshift")
+
+    try:
+        client.delete_cluster_snapshot(
+            SnapshotIdentifier=f"{REDSHIFT_CLUSTER_IDENTIFIER}-snapshot",
+            SnapshotClusterIdentifier=REDSHIFT_CLUSTER_IDENTIFIER,
+        )
+
+        while True:
+            if get_snapshot_status() == "deleting":
+                time.sleep(30)
+                continue
+
+        while True:
+            if get_cluster_status() == "available":
+                time.sleep(30)
+                continue
+    except ClientError as e:
+        logging.info("%s", e)
+        return None
+
+
+def delete_redshift_cluster() -> None:
+    """Delete a redshift cluster and wait until it completely removed"""
+    client = boto3.client("redshift")
+
+    try:
+        client.delete_cluster(
+            ClusterIdentifier=REDSHIFT_CLUSTER_IDENTIFIER,
+            SkipFinalClusterSnapshot=True,
+        )
+
+        while True:
+            if get_cluster_status() == "deleting":
+                time.sleep(30)
+                continue
+    except ClientError as e:
+        logging.info("%s", e)
+        return None
+
 
 with DAG(
     dag_id="example_async_redshift_cluster_management",
@@ -28,6 +157,23 @@ with DAG(
     tags=["example", "async", "redshift"],
 ) as dag:
     start = DummyOperator(task_id="start")
+
+    config = BashOperator(
+        task_id="aws_config",
+        bash_command=f"aws configure set aws_access_key_id {AWS_ACCESS_KEY_ID}; "
+        f"aws configure set aws_secret_access_key {AWS_SECRET_ACCESS_KEY}; "
+        f"aws configure set default.region {AWS_DEFAULT_REGION}; ",
+    )
+
+    create_cluster_op = PythonOperator(
+        task_id="create_redshift_cluster",
+        python_callable=create_redshift_cluster,
+    )
+
+    create_cluster_snapshot_op = PythonOperator(
+        task_id="create_cluster_snapshot",
+        python_callable=create_redshift_cluster_snapshot,
+    )
 
     # [START howto_operator_redshift_pause_cluster_async]
     pause_cluster_task = RedshiftPauseClusterOperatorAsync(
@@ -54,6 +200,30 @@ with DAG(
     )
     # [START howto_operator_redshift_cluster_sensor_async]
 
+    delete_cluster_snapshot_op = PythonOperator(
+        task_id="delete_redshift_cluster_snapshot",
+        python_callable=delete_redshift_cluster_snapshot,
+        trigger_rule="all_done",
+    )
+
+    delete_cluster_op = PythonOperator(
+        task_id="delete_redshift_cluster",
+        python_callable=delete_redshift_cluster,
+        trigger_rule="all_done",
+    )
+
     end = DummyOperator(task_id="end")
 
-    start >> pause_cluster_task >> [resume_cluster_task, async_redshift_sensor_task] >> end
+    (
+        start
+        >> config
+        >> create_cluster_op
+        >> create_cluster_snapshot_op
+        >> pause_cluster_task
+        >> [resume_cluster_task, async_redshift_sensor_task]
+        >> delete_cluster_snapshot_op
+        >> delete_cluster_op
+    )
+
+    [resume_cluster_task, async_redshift_sensor_task] >> end
+    [delete_cluster_snapshot_op, delete_cluster_op] >> end
--- astronomer/providers/amazon/aws/example_dags/example_s3.py
+++ astronomer/providers/amazon/aws/example_dags/example_s3.py
@@ -22,11 +22,13 @@ S3_BUCKET_KEY = os.environ.get("S3_BUCKET_KEY", "test/example_s3_test_file.txt")
 S3_BUCKET_WILDCARD_KEY = os.environ.get("S3_BUCKET_WILDCARD_KEY", "test*")
 PREFIX = os.environ.get("S3_PREFIX", "test")
 INACTIVITY_PERIOD = float(os.environ.get("INACTIVITY_PERIOD", 5))
-REGION_NAME = os.environ.get("REGION_NAME", "us-east-2")
+AWS_DEFAULT_REGION = os.environ.get("AWS_DEFAULT_REGION", "us-east-1")
 LOCAL_FILE_PATH = os.environ.get("LOCAL_FILE_PATH", "/usr/local/airflow/dags/example_s3_test_file.txt")
 AWS_CONN_ID = os.environ.get("ASTRO_AWS_S3_CONN_ID", "aws_s3_default")
 
 default_args = {
+    "retry": 5,
+    "retry_delay": timedelta(minutes=1),
     "execution_timeout": timedelta(minutes=30),
 }
 
@@ -40,7 +42,7 @@ with DAG(
 ) as dag:
     create_bucket = S3CreateBucketOperator(
         task_id="create_bucket",
-        region_name=REGION_NAME,
+        region_name=AWS_DEFAULT_REGION,
         bucket_name=S3_BUCKET_NAME,
         aws_conn_id=AWS_CONN_ID,
     )
