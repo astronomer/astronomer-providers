62f2d608dd9c38875fda103dadf36ac60155c91a
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-04-07 17:11:35 +0545

    remove the liscense and add tag to example dag

--- astronomer/providers/apache/livy/example_dags/example_livy.py
+++ astronomer/providers/apache/livy/example_dags/example_livy.py
@@ -1,20 +1,3 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
 """
 This is an example DAG which uses the LivyOperatorAsync.
 The tasks below trigger the computation of pi on the Spark instance
@@ -33,9 +16,10 @@ LIVY_PYTHON_FILE = os.environ.get("LIVY_PYTHON_FILE", "/user/hadoop/pi.py")
 with DAG(
     dag_id="example_livy_operator",
     default_args={"args": [10]},
-    schedule_interval="@daily",
+    schedule_interval=None,
     start_date=datetime(2021, 1, 1),
     catchup=False,
+    tags=["example", "async", "deferrable", "LivyOperatorAsync"],
 ) as dag:
 
     # [START create_livy]
