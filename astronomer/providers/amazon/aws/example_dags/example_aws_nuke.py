8310221a203da1adb55d5c9f8c869e3d68a22e95
Phani Kumar <94376113+phanikumv@users.noreply.github.com>
2022-04-29 10:56:46 +0530

    Enable `aws-nuke` to delete specified AWS resources (#283)

---
+++ astronomer/providers/amazon/aws/example_dags/example_aws_nuke.py
@@ -0,0 +1,42 @@
+import os
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "**********")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "***********")
+AWS_DEFAULT_REGION = os.environ.get("AWS_DEFAULT_REGION", "us-east-2")
+AWS_CONN_ID = os.environ.get("ASTRO_AWS_CONN_ID", "aws_default")
+EXECUTION_TIMEOUT = int(os.getenv("EXECUTION_TIMEOUT", 6))
+
+default_args = {
+    "execution_timeout": timedelta(hours=EXECUTION_TIMEOUT),
+}
+
+with DAG(
+    dag_id="example_aws_nuke",
+    start_date=datetime(2022, 1, 1),
+    schedule_interval="30 20 * * *",
+    catchup=False,
+    default_args=default_args,
+    tags=["example", "aws-nuke"],
+) as dag:
+    start = DummyOperator(task_id="start")
+
+    set_aws_config = BashOperator(
+        task_id="aws_config",
+        bash_command=f"aws configure set aws_access_key_id {AWS_ACCESS_KEY_ID}; "
+        f"aws configure set aws_secret_access_key {AWS_SECRET_ACCESS_KEY}; "
+        f"aws configure set default.region {AWS_DEFAULT_REGION}; ",
+    )
+
+    execute_aws_nuke = BashOperator(
+        task_id="execute_aws_nuke",
+        bash_command="aws-nuke -c /usr/local/airflow/dags/nuke-config.yml --profile default --force --no-dry-run; ",
+    )
+
+    end = DummyOperator(task_id="end")
+
+    start >> set_aws_config >> execute_aws_nuke >> end
