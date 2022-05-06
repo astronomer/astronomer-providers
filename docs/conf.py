f1be2f6d57a07e2e7116d593e7b8fba787219e3a
Kaxil Naik <kaxilnaik@gmail.com>
2022-05-06 20:58:43 +0100

    Publish `1.3.0b1` (#326)
    This commit publishes 1.3.0beta1 to allow testing with Airflow 2.3 and new version of Providers
    
    Co-Authored-By: Phani Kumar <94376113+phanikumv@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = "Astronomer Providers"
 author = "Astronomer Inc."
 
 # The full version, including alpha/beta/rc tags
-release = "1.3.0.dev1"
+release = "1.3.0b1"
 
 
 # -- General configuration ---------------------------------------------------
