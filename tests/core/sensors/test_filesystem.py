985e5ff61bd1fa3f69827253fba09f7bbb4d73db
Pankaj <pankaj.singh@astronomer.io>
2022-05-12 23:59:43 +0530

    Update core module unit test to classe based test

--- tests/core/sensors/test_filesystem.py
+++ tests/core/sensors/test_filesystem.py
@@ -1,28 +1,25 @@
 import pytest
 from airflow.exceptions import TaskDeferred
-from airflow.utils.timezone import datetime
 
 from astronomer.providers.core.sensors.filesystem import FileSensorAsync
 from astronomer.providers.core.triggers.filesystem import FileTrigger
 
-DEFAULT_DATE = datetime(2015, 1, 1)
-TASK_ID = "example_file_sensor_task"
-TEST_POLL_INTERVAL = 3.0
-TEST_FILE_PATH = "/tmp/sample.py"
 
+class TestFileSensorAsync:
+    FILE_PATH = "/tmp/sample.py"
 
-def test_filesystem_sensor_async(dag, context):
-    """
-    Asserts that a task is deferred and an FileTrigger will be fired
-    when FileSensorAsync is provided with all required arguments
-    """
-    sensor = FileSensorAsync(
-        filepath=TEST_FILE_PATH,
-        task_id=TASK_ID,
-        dag=dag,
-    )
+    def test_task_defer(self, dag, context):
+        """
+        Asserts that a task is deferred and an FileTrigger will be fired
+        when FileSensorAsync is provided with all required arguments
+        """
+        task = FileSensorAsync(
+            task_id="wait_for_file",
+            filepath=self.FILE_PATH,
+            dag=dag,
+        )
 
-    with pytest.raises(TaskDeferred) as exc:
-        sensor.execute(context)
+        with pytest.raises(TaskDeferred) as exc:
+            task.execute(context)
 
-    assert isinstance(exc.value.trigger, FileTrigger), "Trigger is not a FileTrigger"
+        assert isinstance(exc.value.trigger, FileTrigger), "Trigger is not a FileTrigger"
--- tests/core/triggers/test_filesystem.py
+++ tests/core/triggers/test_filesystem.py
@@ -1,57 +1,48 @@
 import asyncio
-import sys
 
 import pytest
 
 from astronomer.providers.core.triggers.filesystem import FileTrigger
 
-TEST_POLL_INTERVAL = 3.0
 
+class TestFileTrigger:
+    FILE_PATH = "/files/dags/example_async_file.py"
+    POLL_INTERVAL = 3.0
 
-def test_filesystem_trigger_serialization():
-    """
-    Asserts that the TaskStateTrigger correctly serializes its arguments
-    and classpath.
-    """
-    trigger = FileTrigger(filepath="/files/dags/example_async_file.py", poll_interval=TEST_POLL_INTERVAL)
-    classpath, kwargs = trigger.serialize()
-    assert classpath == "astronomer.providers.core.triggers.filesystem.FileTrigger"
-    assert kwargs == {
-        "filepath": "/files/dags/example_async_file.py",
-        "poll_interval": TEST_POLL_INTERVAL,
-        "recursive": False,
-    }
+    def test_serialization(self):
+        """Asserts that the TaskStateTrigger correctly serializes its arguments and classpath."""
+        trigger = FileTrigger(filepath=self.FILE_PATH, poll_interval=self.POLL_INTERVAL)
+        classpath, kwargs = trigger.serialize()
+        assert classpath == "astronomer.providers.core.triggers.filesystem.FileTrigger"
+        assert kwargs == {
+            "filepath": self.FILE_PATH,
+            "poll_interval": self.POLL_INTERVAL,
+            "recursive": False,
+        }
 
+    @pytest.mark.asyncio
+    async def test_task_file_trigger(self, tmp_path):
+        """Asserts that the FileTrigger only goes off on or after file is found"""
+        tmp_dir = tmp_path / "test_dir"
+        tmp_dir.mkdir()
+        p = tmp_dir / "hello.txt"
+        p.unlink(missing_ok=True)
 
-@pytest.mark.skipif(
-    sys.version_info.minor <= 6 and sys.version_info.major <= 3,
-    reason="No async on 3.6",
-)
-@pytest.mark.asyncio
-async def test_task_file_trigger(tmp_path):
-    """
-    Asserts that the FileTrigger only goes off on or after file is found
-    """
-    tmp_dir = tmp_path / "test_dir"
-    tmp_dir.mkdir()
-    p = tmp_dir / "hello.txt"
-    p.unlink(missing_ok=True)
+        trigger = FileTrigger(
+            filepath=str(p.resolve()),
+            poll_interval=0.2,
+        )
 
-    trigger = FileTrigger(
-        filepath=str(p.resolve()),
-        poll_interval=0.2,
-    )
+        task = asyncio.create_task(trigger.run().__anext__())
+        await asyncio.sleep(0.5)
 
-    task = asyncio.create_task(trigger.run().__anext__())
-    await asyncio.sleep(0.5)
+        # It should not have produced a result
+        assert task.done() is False
 
-    # It should not have produced a result
-    assert task.done() is False
+        p.touch()
 
-    p.touch()
+        await asyncio.sleep(0.5)
+        assert task.done() is True
 
-    await asyncio.sleep(0.5)
-    assert task.done() is True
-
-    # Prevents error when task is destroyed while in "pending" state
-    asyncio.get_event_loop().stop()
+        # Prevents error when task is destroyed while in "pending" state
+        asyncio.get_event_loop().stop()
