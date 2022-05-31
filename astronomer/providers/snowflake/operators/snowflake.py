dcb68fb5d2aba0c41277775281bcde4422830a7c
bharanidharan14 <bharanidharan.manimuthu@astronomer.io>
2022-05-31 10:19:34 +0530

    Add Query Tag in snowflake operator
    - Fixed code coverage failure and improved the code coverage percentage
    
    Add Query Tag in snowflake operator
    
    Add Query Tag
    
    Added Query tag to the snowflake operator by adding this the Openlineage adapter for Snowflake can directly read audit info. To add to the richness of the information available for queries run with Airflow
    
    Fix test case failure
    
    Fix test case failure

--- astronomer/providers/snowflake/operators/snowflake.py
+++ astronomer/providers/snowflake/operators/snowflake.py
@@ -56,6 +56,17 @@ class SnowflakeOperatorAsync(SnowflakeOperator):
         By deferring the SnowflakeTrigger class pass along with query ids.
         """
         self.log.info("Executing: %s", self.sql)
+
+        default_query_tag = f"airflow_openlineage_{self.task_id}_{self.dag.dag_id}_{context['ti'].try_number}"
+        query_tag = (
+            self.parameters.get("query_tag", default_query_tag) if self.parameters else default_query_tag
+        )
+        session_query_tag = f"ALTER SESSION SET query_tag = '{query_tag}';"
+        if isinstance(self.sql, str):
+            self.sql = "\n".join([session_query_tag, self.sql])
+        else:
+            [session_query_tag].extend(self.sql)
+
         hook = self.get_db_hook()
         hook.run(self.sql, autocommit=self.autocommit, parameters=self.parameters)
         self.query_ids = hook.query_ids
