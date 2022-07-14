fec510075e6f63f39db946910affbe7778e6e646
Pankaj Koti <pankaj.koti@astronomer.io>
2022-07-14 18:45:07 +0530

    Rename tests directory operators->sensors for http & increase coverage

--- tests/http/sensors/test_http.py
+++ tests/http/sensors/test_http.py
@@ -7,19 +7,19 @@ from astronomer.providers.http.sensors.http import HttpSensorAsync
 from astronomer.providers.http.triggers.http import HttpTrigger
 
 
-def test_http_run_now_operator_async():
+def test_http_run_now_sensor_async():
     """
     Asserts that a task is deferred and a HttpTrigger will be fired
     when the HttpSensorAsync is executed.
     """
 
-    operator = HttpSensorAsync(
+    task = HttpSensorAsync(
         task_id="run_now",
         endpoint="test-endpoint",
     )
 
     with pytest.raises(TaskDeferred) as exc:
-        operator.execute({})
+        task.execute({})
 
     assert isinstance(exc.value.trigger, HttpTrigger), "Trigger is not a HttpTrigger"
 
@@ -29,7 +29,7 @@ def test_http_response_check_does_not_run_async():
     Asserts that a task is not deferred when response_check arg is passed to HttpSensorAsync.
     """
 
-    operator = HttpSensorAsync(
+    task = HttpSensorAsync(
         task_id="run_now",
         endpoint="test-endpoint",
         response_check=lambda response: "httpbin" in response.text,
@@ -38,15 +38,25 @@ def test_http_response_check_does_not_run_async():
     with mock.patch("astronomer.providers.http.sensors.http.HttpSensorAsync.defer") as mock_defer, mock.patch(
         "airflow.sensors.base.BaseSensorOperator.execute"
     ):
-        operator.execute({})
+        task.execute({})
 
     mock_defer.assert_not_called()
 
-    operator = HttpSensorAsync(
+    task = HttpSensorAsync(
         task_id="run_now",
         endpoint="test-endpoint",
     )
 
     with mock.patch("astronomer.providers.http.sensors.http.HttpSensorAsync.defer") as mock_defer:
-        operator.execute({})
+        task.execute({})
         mock_defer.assert_called_once_with(timeout=None, trigger=mock.ANY, method_name="execute_complete")
+
+
+@mock.patch("astronomer.providers.http.sensors.http.HttpSensor")
+def test_http_sensor_async_hook_initialisation_attribute_error(mock_http_sensor):
+    """
+    Asserts that an attribute error that may be raised across different versions of the HTTP provider is handled
+    while initialising the hook in the sensor.
+    """
+    mock_http_sensor.side_effect = AttributeError()
+    HttpSensorAsync(task_id="check_hook_initialisation", endpoint="")
