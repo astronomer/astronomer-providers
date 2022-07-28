c9c5e12260085811d0aeed0f5854db80828539f8
Rajath <92459020+rajaths010494@users.noreply.github.com>
2022-07-28 19:18:30 +0530

    Fix unit test case

--- tests/amazon/aws/operators/test_redshift_sql.py
+++ tests/amazon/aws/operators/test_redshift_sql.py
@@ -1,7 +1,12 @@
+import datetime
 from unittest import mock
 
 import pytest
 from airflow.exceptions import AirflowException, TaskDeferred
+from airflow.models.dag import DAG
+from airflow.models.dagrun import DagRun
+from airflow.models.taskinstance import TaskInstance
+from airflow.utils.types import DagRunType
 
 from astronomer.providers.amazon.aws.operators.redshift_sql import (
     RedshiftSQLOperatorAsync,
@@ -18,11 +23,34 @@ def context():
     """
     Creates an empty context.
     """
-    yield
+    context = {}
+    yield context
+
+
+def create_context(task, dag):
+    execution_date = datetime.datetime(2022, 1, 1, 0, 0, 0)
+    dag_run = DagRun(
+        dag_id=dag.dag_id,
+        execution_date=execution_date,
+        run_id=DagRun.generate_run_id(DagRunType.MANUAL, execution_date),
+    )
+    task_instance = TaskInstance(task=task)
+    task_instance.dag_run = dag_run
+    task_instance.dag_id = dag.dag_id
+    task_instance.xcom_push = mock.Mock()
+    return {
+        "dag": dag,
+        "run_id": dag_run.run_id,
+        "task": task,
+        "ti": task_instance,
+        "task_instance": task_instance,
+    }
 
 
 @mock.patch("astronomer.providers.amazon.aws.hooks.redshift_data.RedshiftDataHook.execute_query")
 def test_redshiftsql_op_async(mock_execute):
+    args = {"owner": "airflow", "start_date": datetime.datetime(2017, 1, 1)}
+    dag = DAG("test_redshift_sql_async_execute_complete", default_args=args)
     mock_execute.return_value = [], {}
     task = RedshiftSQLOperatorAsync(
         task_id=TEST_TASK_ID,
@@ -30,7 +58,7 @@ def test_redshiftsql_op_async(mock_execute):
         params=TEST_PARAMATERS,
     )
     with pytest.raises(TaskDeferred) as exc:
-        task.execute(context)
+        task.execute(create_context(task, dag))
     assert isinstance(exc.value.trigger, RedshiftSQLTrigger), "Trigger is not a RedshiftSQLTrigger"
 
 
