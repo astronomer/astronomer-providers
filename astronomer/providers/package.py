6fae417d5563d0ec60dcdd8249d1dd15fb079468
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2022-09-13 15:30:37 +0530

    Add hyperlink for provider package description (#638)

--- astronomer/providers/package.py
+++ astronomer/providers/package.py
@@ -5,6 +5,8 @@ from typing import Any, Dict
 config = configparser.ConfigParser(strict=False)
 _REPO_DIR = Path(__file__).parent.parent.parent
 config.read(_REPO_DIR / "setup.cfg")
+_description = config.get("metadata", "description", fallback="")
+_description_link = f"`{_description} <https://github.com/astronomer/astronomer-providers/>`__"
 
 
 def get_provider_info() -> Dict[str, Any]:
@@ -13,7 +15,7 @@ def get_provider_info() -> Dict[str, Any]:
         # Required.
         "package-name": "astronomer-providers",
         "name": "Astronomer Providers",
-        "description": (config.get("metadata", "description", fallback="")),
+        "description": (_description_link),
         "versions": [config.get("metadata", "version", fallback="")],
         # Optional.
         "hook-class-names": [],
