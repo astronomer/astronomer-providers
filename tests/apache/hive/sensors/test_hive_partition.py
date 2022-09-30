44d2972302a3a189573b914b92c897fa7e847a94
Pankaj <pankaj.singh@astronomer.io>
2022-10-01 02:16:31 +0530

    Apache tests cleanup
    Fix tests

--- tests/apache/hive/sensors/test_hive_partition.py
+++ tests/apache/hive/sensors/test_hive_partition.py
@@ -15,63 +15,54 @@ TEST_PARTITION = "state='FL'"
 TEST_METASTORE_CONN_ID = "test_metastore_default"
 
 
-@pytest.fixture()
-def context():
-    """Creates an empty context."""
-    context = {}
-    yield context
+class TestHivePartitionSensorAsync:
+    def test_hive_partition_sensor_async(self, context):
+        """
+        Asserts that a task is deferred and a HivePartitionTrigger will be fired
+        when the HivePartitionSensorAsync is executed.
+        """
+        task = HivePartitionSensorAsync(
+            task_id="task-id",
+            table=TEST_TABLE,
+            partition=TEST_PARTITION,
+            metastore_conn_id=TEST_METASTORE_CONN_ID,
+        )
+        with pytest.raises(TaskDeferred) as exc:
+            task.execute(context)
+        assert isinstance(exc.value.trigger, HivePartitionTrigger), "Trigger is not a HivePartitionTrigger"
 
+    def test_hive_partition_sensor_async_execute_failure(self, context):
+        """Tests that an AirflowException is raised in case of error event"""
+        task = HivePartitionSensorAsync(
+            task_id="task-id",
+            table=TEST_TABLE,
+            partition=TEST_PARTITION,
+            metastore_conn_id=TEST_METASTORE_CONN_ID,
+        )
+        with pytest.raises(AirflowException):
+            task.execute_complete(context=None, event={"status": "error", "message": "test failure message"})
 
-def test_hive_partition_sensor_async():
-    """
-    Asserts that a task is deferred and a HivePartitionTrigger will be fired
-    when the HivePartitionSensorAsync is executed.
-    """
-    task = HivePartitionSensorAsync(
-        task_id="task-id",
-        table=TEST_TABLE,
-        partition=TEST_PARTITION,
-        metastore_conn_id=TEST_METASTORE_CONN_ID,
-    )
-    with pytest.raises(TaskDeferred) as exc:
-        task.execute(context)
-    assert isinstance(exc.value.trigger, HivePartitionTrigger), "Trigger is not a HivePartitionTrigger"
+    def test_hive_partition_sensor_async_execute_complete(self):
+        """Asserts that logging occurs as expected"""
+        task = HivePartitionSensorAsync(
+            task_id="task-id",
+            table=TEST_TABLE,
+            partition=TEST_PARTITION,
+            metastore_conn_id=TEST_METASTORE_CONN_ID,
+        )
+        with mock.patch.object(task.log, "info") as mock_log_info:
+            task.execute_complete(context=None, event={"status": "success", "message": "Job completed"})
+        mock_log_info.assert_called_with(
+            "Success criteria met. Found partition %s in table: %s", TEST_PARTITION, TEST_TABLE
+        )
 
-
-def test_hive_partition_sensor_async_execute_failure(context):
-    """Tests that an AirflowException is raised in case of error event"""
-    task = HivePartitionSensorAsync(
-        task_id="task-id",
-        table=TEST_TABLE,
-        partition=TEST_PARTITION,
-        metastore_conn_id=TEST_METASTORE_CONN_ID,
-    )
-    with pytest.raises(AirflowException):
-        task.execute_complete(context=None, event={"status": "error", "message": "test failure message"})
-
-
-def test_hive_partition_sensor_async_execute_complete():
-    """Asserts that logging occurs as expected"""
-    task = HivePartitionSensorAsync(
-        task_id="task-id",
-        table=TEST_TABLE,
-        partition=TEST_PARTITION,
-        metastore_conn_id=TEST_METASTORE_CONN_ID,
-    )
-    with mock.patch.object(task.log, "info") as mock_log_info:
-        task.execute_complete(context=None, event={"status": "success", "message": "Job completed"})
-    mock_log_info.assert_called_with(
-        "Success criteria met. Found partition %s in table: %s", TEST_PARTITION, TEST_TABLE
-    )
-
-
-def test_hive_partition_sensor_async_execute_failure_no_event(context):
-    """Tests that an AirflowException is raised in case of no event"""
-    task = HivePartitionSensorAsync(
-        task_id="task-id",
-        table=TEST_TABLE,
-        partition=TEST_PARTITION,
-        metastore_conn_id=TEST_METASTORE_CONN_ID,
-    )
-    with pytest.raises(AirflowException):
-        task.execute_complete(context=None, event=None)
+    def test_hive_partition_sensor_async_execute_failure_no_event(self, context):
+        """Tests that an AirflowException is raised in case of no event"""
+        task = HivePartitionSensorAsync(
+            task_id="task-id",
+            table=TEST_TABLE,
+            partition=TEST_PARTITION,
+            metastore_conn_id=TEST_METASTORE_CONN_ID,
+        )
+        with pytest.raises(AirflowException):
+            task.execute_complete(context=None, event=None)
--- tests/apache/livy/operators/test_livy.py
+++ tests/apache/livy/operators/test_livy.py
@@ -1,12 +1,9 @@
-import unittest
 from unittest.mock import MagicMock, patch
 
 import pytest
 from airflow.exceptions import AirflowException, TaskDeferred
-from airflow.models import Connection
-from airflow.models.dag import DAG
 from airflow.providers.apache.livy.hooks.livy import BatchState
-from airflow.utils import db, timezone
+from airflow.utils import timezone
 
 from astronomer.providers.apache.livy.operators.livy import LivyOperatorAsync
 from astronomer.providers.apache.livy.triggers.livy import LivyTrigger
@@ -18,22 +15,14 @@ BATCH_ID = 100
 LOG_RESPONSE = {"total": 3, "log": ["first_line", "second_line", "third_line"]}
 
 
-class TestLivyOperatorAsync(unittest.TestCase):
-    def setUp(self):
-        args = {"owner": "airflow", "start_date": DEFAULT_DATE}
-        self.dag = DAG("test_dag_id", default_args=args)
-        db.merge_conn(
-            Connection(
-                conn_id="livyunittest", conn_type="livy", host="localhost:8998", port="8998", schema="http"
-            )
-        )
-
+class TestLivyOperatorAsync:
+    @pytest.fixture()
     @patch(
         "astronomer.providers.apache.livy.hooks.livy.LivyHookAsync.dump_batch_logs",
         return_value=None,
     )
     @patch("astronomer.providers.apache.livy.hooks.livy.LivyHookAsync.get_batch_state")
-    async def test_poll_for_termination(self, mock_livy, mock_dump_logs):
+    async def test_poll_for_termination(self, mock_livy, mock_dump_logs, dag):
         state_list = 2 * [BatchState.RUNNING] + [BatchState.SUCCESS]
 
         def side_effect(_, retry_args):
@@ -44,7 +33,7 @@ class TestLivyOperatorAsync(unittest.TestCase):
 
         mock_livy.side_effect = side_effect
 
-        task = LivyOperatorAsync(file="sparkapp", polling_interval=1, dag=self.dag, task_id="livy_example")
+        task = LivyOperatorAsync(file="sparkapp", polling_interval=1, dag=dag, task_id="livy_example")
         task._livy_hook = task.get_hook()
         task.poll_for_termination(BATCH_ID)
 
@@ -53,12 +42,12 @@ class TestLivyOperatorAsync(unittest.TestCase):
         assert mock_livy.call_count == 3
 
     @patch("airflow.providers.apache.livy.operators.livy.LivyHook.post_batch", return_value=BATCH_ID)
-    def test_livy_operator_async(self, mock_post):
+    def test_livy_operator_async(self, mock_post, dag):
         task = LivyOperatorAsync(
             livy_conn_id="livyunittest",
             file="sparkapp",
             polling_interval=1,
-            dag=self.dag,
+            dag=dag,
             task_id="livy_example",
         )
 
@@ -68,16 +57,14 @@ class TestLivyOperatorAsync(unittest.TestCase):
         assert isinstance(exc.value.trigger, LivyTrigger), "Trigger is not a LivyTrigger"
 
     @patch("airflow.providers.apache.livy.operators.livy.LivyHook.post_batch", return_value=BATCH_ID)
-    def test_livy_operator_async_execute_complete_success(self, mock_post):
-        """
-        Asserts that a task is completed with success status.
-        """
+    def test_livy_operator_async_execute_complete_success(self, mock_post, dag):
+        """Asserts that a task is completed with success status."""
 
         task = LivyOperatorAsync(
             livy_conn_id="livyunittest",
             file="sparkapp",
             polling_interval=1,
-            dag=self.dag,
+            dag=dag,
             task_id="livy_example",
         )
         assert (
@@ -94,16 +81,14 @@ class TestLivyOperatorAsync(unittest.TestCase):
         )
 
     @patch("airflow.providers.apache.livy.operators.livy.LivyHook.post_batch", return_value=BATCH_ID)
-    def test_livy_operator_async_execute_complete_error(self, mock_post):
-        """
-        Asserts that a task is completed with success status.
-        """
+    def test_livy_operator_async_execute_complete_error(self, mock_post, dag):
+        """Asserts that a task is completed with success status."""
 
         task = LivyOperatorAsync(
             livy_conn_id="livyunittest",
             file="sparkapp",
             polling_interval=1,
-            dag=self.dag,
+            dag=dag,
             task_id="livy_example",
         )
         with pytest.raises(AirflowException):
