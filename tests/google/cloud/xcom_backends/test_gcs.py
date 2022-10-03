42801da8878395ebdf6b1bd3440fcd81d3ccb83a
rajaths010494 <rajath.srinivasaiah@astronomer.io>
2022-10-03 17:11:01 +0530

    Fix code coverage

--- tests/google/cloud/xcom_backends/test_gcs.py
+++ tests/google/cloud/xcom_backends/test_gcs.py
@@ -1,11 +1,53 @@
+from __future__ import annotations
+
+import contextlib
 import json
+import os
+import pickle  # nosec
 from unittest import mock
 
 import pandas as pd
 import pytest
+from airflow import settings
+from airflow.configuration import conf
 from airflow.models.xcom import BaseXCom
+from pandas.util.testing import assert_frame_equal
 
 from astronomer.providers.google.cloud.xcom_backends.gcs import GCSXComBackend
+
+
+@contextlib.contextmanager
+def conf_vars(overrides):
+    original = {}
+    original_env_vars = {}
+    for (section, key), value in overrides.items():
+
+        env = conf._env_var_name(section, key)
+        if env in os.environ:
+            original_env_vars[env] = os.environ.pop(env)
+
+        if conf.has_option(section, key):
+            original[(section, key)] = conf.get(section, key)
+        else:
+            original[(section, key)] = None
+        if value is not None:
+            if not conf.has_section(section):
+                conf.add_section(section)
+            conf.set(section, key, value)
+        else:
+            conf.remove_option(section, key)
+    settings.configure_vars()
+    try:
+        yield
+    finally:
+        for (section, key), value in original.items():
+            if value is not None:
+                conf.set(section, key, value)
+            else:
+                conf.remove_option(section, key)
+        for env, value in original_env_vars.items():
+            os.environ[env] = value
+        settings.configure_vars()
 
 
 @mock.patch("astronomer.providers.google.cloud.xcom_backends.gcs.GCSXComBackend.write_and_upload_value")
@@ -26,6 +68,22 @@ def test_custom_xcom_gcs_serialize(mock_write):
 @mock.patch("uuid.uuid4")
 @mock.patch("airflow.providers.google.cloud.hooks.gcs.GCSHook.upload")
 def test_custom_xcom_gcs_write_and_upload(mock_upload, mock_uuid, job_id):
+    """
+    Asserts that custom xcom is upload and returns key
+    """
+    mock_uuid.return_value = "12345667890"
+    result = GCSXComBackend.write_and_upload_value(job_id)
+    assert result == "gcs_xcom_" + "12345667890"
+
+
+@conf_vars({("core", "enable_xcom_pickling"): "True"})
+@pytest.mark.parametrize(
+    "job_id",
+    ["1234567890", {"a": "b"}, ["123"]],
+)
+@mock.patch("uuid.uuid4")
+@mock.patch("airflow.providers.google.cloud.hooks.gcs.GCSHook.upload")
+def test_custom_xcom_gcs_write_and_upload_pickle(mock_upload, mock_uuid, job_id):
     """
     Asserts that custom xcom is upload and returns key
     """
@@ -55,6 +113,37 @@ def test_custom_xcom_gcs_deserialize(mock_download, job_id):
     Asserts that custom xcom is deserialized and check for data
     """
     mock_download.return_value = json.dumps(job_id).encode("UTF-8")
+    real_job_id = BaseXCom(value=json.dumps(job_id).encode("UTF-8"))
+    result = GCSXComBackend.deserialize_value(real_job_id)
+    assert result == job_id
+
+
+@pytest.mark.parametrize(
+    "job_id",
+    ["gcs_xcom_1234_dataframe"],
+)
+@mock.patch("astronomer.providers.google.cloud.xcom_backends.gcs.GCSXComBackend.read_value")
+def test_custom_xcom_gcs_deserialize_pandas(mock_read_value, job_id):
+    """
+    Asserts that custom xcom is deserialized and check for data
+    """
+    mock_read_value.return_value = pd.DataFrame({"numbers": [1], "colors": ["red"]}).to_json()
+    real_job_id = BaseXCom(value=json.dumps(job_id).encode("UTF-8"))
+    result = GCSXComBackend.deserialize_value(real_job_id)
+    assert_frame_equal(result, pd.DataFrame({"numbers": [1], "colors": ["red"]}))
+
+
+@conf_vars({("core", "enable_xcom_pickling"): "True"})
+@pytest.mark.parametrize(
+    "job_id",
+    ["gcs_xcom_1234"],
+)
+@mock.patch("airflow.providers.google.cloud.hooks.gcs.GCSHook.download")
+def test_custom_xcom_gcs_deserialize_pickle(mock_download, job_id):
+    """
+    Asserts that custom xcom is deserialized and check for data
+    """
+    mock_download.return_value = pickle.dumps(job_id)
     real_job_id = BaseXCom(value=json.dumps(job_id).encode("UTF-8"))
     result = GCSXComBackend.deserialize_value(real_job_id)
     assert result == job_id
