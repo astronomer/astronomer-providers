fe853fd10e3cc3f59e660ac77d1218e9ad8c5590
bharanidharan14 <bharanidharan.manimuthu@astronomer.io>
2022-10-31 12:37:05 +0530

    Bump version to 1.12.0.dev1

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ project = "Astronomer Providers"
 author = "Astronomer Inc."
 
 # The full version, including alpha/beta/rc tags
-release = "1.11.1"
+release = "1.12.0.dev1"
 
 
 # -- General configuration ---------------------------------------------------
