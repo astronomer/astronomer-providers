fce41129115ceb9b3438f669560d2d8d79f3b5ef
Pankaj Koti <pankajkoti699@gmail.com>
2022-11-16 11:31:32 +0530

    Correct import path

--- astronomer/providers/google/cloud/hooks/bigquery.py
+++ astronomer/providers/google/cloud/hooks/bigquery.py
@@ -13,7 +13,7 @@ try:
     from airflow.providers.google.cloud.utils.bigquery import bq_cast
 except ImportError:
     # For apache-airflow-providers-google < 8.5.0
-    from airflow.providers.google.cloud.utils.bigquery import _bq_cast as bq_cast
+    from airflow.providers.google.cloud.hooks.bigquery import _bq_cast as bq_cast
 
 BigQueryJob = Union[CopyJob, QueryJob, LoadJob, ExtractJob]
 
