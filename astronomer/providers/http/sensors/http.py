d688ef384469d80cca850a4238f3cd212bfe55bd
rajaths010494 <rajath.srinivasaiah@astronomer.io>
2022-11-30 15:39:13 +0530

    Add warning log and update doc string

--- astronomer/providers/http/sensors/http.py
+++ astronomer/providers/http/sensors/http.py
@@ -14,6 +14,24 @@ class HttpSensorAsync(HttpSensor):
     Executes a HTTP GET statement and returns False on failure caused by
     404 Not Found or `response_check` returning False.
 
+    .. note::
+        If ``response_check`` is passed, the sync version of the sensor will be used.
+
+    The response check can access the template context to the operator:
+
+    .. code-block:: python
+
+        def response_check(response, task_instance):
+            # The task_instance is injected, so you can pull data form xcom
+            # Other context variables such as dag, ds, execution_date are also available.
+            xcom_data = task_instance.xcom_pull(task_ids="pushing_task")
+            # In practice you would do something more sensible with this data..
+            print(xcom_data)
+            return True
+
+
+        HttpSensorAsync(task_id="my_http_sensor", ..., response_check=response_check)
+
     :param http_conn_id: The Connection ID to run the sensor against
     :type http_conn_id: str
     :param method: The HTTP request method to use
@@ -24,11 +42,11 @@ class HttpSensorAsync(HttpSensor):
     :type request_params: a dictionary of string key/value pairs
     :param headers: The HTTP headers to be added to the GET request
     :type headers: a dictionary of string key/value pairs
-    :param response_check: This param is currently not supported.
-        A check against the 'requests' response object.
+    :param response_check: A check against the 'requests' response object.
         The callable takes the response object as the first positional argument
         and optionally any number of keyword arguments available in the context dictionary.
         It should return True for 'pass' and False otherwise.
+        Currently if this parameter is specified then sync version of the sensor will be used.
     :type response_check: A lambda or defined function.
     :param extra_options: Extra options for the 'requests' library, see the
         'requests' documentation (options to modify timeout, ssl, etc.)
@@ -83,6 +101,7 @@ class HttpSensorAsync(HttpSensor):
         # Maybe we set method_name as users function??? to run it again
         # and evaluate the response.
         if self.response_check:
+            self.log.warning("Since response_check param is passed, using the sync version of the sensor.")
             super().execute(context=context)
         else:
             self.defer(
