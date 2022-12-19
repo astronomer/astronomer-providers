c8ace80e2c182739e5856eb4e76e3c7843716732
rajaths010494 <rajath.srinivasaiah@astronomer.io>
2022-12-19 11:39:03 +0530

    Bump version to 1.14.0.dev1

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ project = "Astronomer Providers"
 author = "Astronomer Inc."
 
 # The full version, including alpha/beta/rc tags
-release = "1.13.0"
+release = "1.14.0.dev1"
 
 
 # -- General configuration ---------------------------------------------------
