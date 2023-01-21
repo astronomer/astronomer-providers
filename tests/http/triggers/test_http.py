6c1c0ba845f712d89fc765817c4bb86cc0ddab47
Pankaj <pankaj.singh@astronomer.io>
2023-01-22 01:26:35 +0530

    Add http class based tests

--- tests/http/triggers/test_http.py
+++ tests/http/triggers/test_http.py
@@ -1,23 +1,22 @@
 from astronomer.providers.http.triggers.http import HttpTrigger
 
 
-def test_http_trigger_serialization():
-    """
-    Asserts that the HttpTrigger correctly serializes its arguments and classpath.
-    """
-    trigger = HttpTrigger(
-        endpoint="test-endpoint",
-        http_conn_id="http_default",
-        method="GET",
-        headers={"Content-Type": "application/json"},
-    )
-    classpath, kwargs = trigger.serialize()
-    assert classpath == "astronomer.providers.http.triggers.http.HttpTrigger"
-    assert kwargs == {
-        "data": None,
-        "endpoint": "test-endpoint",
-        "extra_options": {},
-        "headers": {"Content-Type": "application/json"},
-        "http_conn_id": "http_default",
-        "poke_interval": 5.0,
-    }
+class TestHttpTrigger:
+    def test_serialization(self):
+        """Asserts that the HttpTrigger correctly serializes its arguments and classpath."""
+        trigger = HttpTrigger(
+            endpoint="test-endpoint",
+            http_conn_id="http_default",
+            method="GET",
+            headers={"Content-Type": "application/json"},
+        )
+        classpath, kwargs = trigger.serialize()
+        assert classpath == "astronomer.providers.http.triggers.http.HttpTrigger"
+        assert kwargs == {
+            "data": None,
+            "endpoint": "test-endpoint",
+            "extra_options": {},
+            "headers": {"Content-Type": "application/json"},
+            "http_conn_id": "http_default",
+            "poke_interval": 5.0,
+        }
