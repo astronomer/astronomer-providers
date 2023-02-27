77c7bf6c5fd8308cc3d88a2c4b3f4a2341c13c91
Pankaj <pankaj.singh@astronomer.io>
2023-02-27 22:03:57 +0530

    Add tests

--- tests/http/triggers/test_http.py
+++ tests/http/triggers/test_http.py
@@ -5,7 +5,7 @@ from astronomer.providers.http.triggers.http import HttpTrigger
 
 class TestHttpTrigger:
     @pytest.mark.parametrize(
-        "input_conn,output_conn", ["http_default", "http_connection"]
+        "conn", ["http_default", "http_connection"]
     )
     def test_serialization(self, conn):
         """Asserts that the HttpTrigger correctly serializes its arguments and classpath."""
