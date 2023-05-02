6d01ee4590dcd5150bfac165cd5bb735e3f29473
Pankaj <pankaj.singh@astronomer.io>
2023-05-02 13:04:14 +0530

    Include AWS nuke DAG in integration test master DAG
    since now we run tests for both KE/CE and both use the same resource so it makes sense to include nuke DAG in master DAG so that cleanup starts as soon as test execution is completed.

--- .circleci/integration-tests/master_dag.py
+++ .circleci/integration-tests/master_dag.py
@@ -242,6 +242,18 @@ with DAG(
         provide_context=True,
     )
 
+    end_test = DummyOperator(
+        task_id="end_test",
+        trigger_rule="all_success",
+    )
+
+    aws_nuke_task_info = [
+        {"aws_nuke_dag": "example_aws_nuke"},
+    ]
+    aws_nuke_trigger_tasks, ids = prepare_dag_dependency(aws_nuke_task_info, "{{ ds }}")
+    dag_run_ids.extend(ids)
+    chain(*aws_nuke_trigger_tasks)
+
     end = DummyOperator(
         task_id="end",
         trigger_rule="all_success",
@@ -287,5 +299,5 @@ with DAG(
         dbt_trigger_tasks[-1],
     ]
 
-    last_task >> end
-    last_task >> report
+    last_task >> end_test
+    last_task >> report >> end_test >> aws_nuke_trigger_tasks[0] >> end
