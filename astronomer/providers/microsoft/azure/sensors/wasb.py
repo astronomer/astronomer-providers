6b8525e1c1791e69736fe33b89e134a6674449c6
Wei Lee <weilee.rx@gmail.com>
2023-05-03 23:31:01 +0800

    feat(microsoft.azure): check state before deferring WasbBlobSensorAsync (#1028)

--- astronomer/providers/microsoft/azure/sensors/wasb.py
+++ astronomer/providers/microsoft/azure/sensors/wasb.py
@@ -53,17 +53,18 @@ class WasbBlobSensorAsync(WasbBlobSensor):
 
     def execute(self, context: Context) -> None:
         """Defers trigger class to poll for state of the job run until it reaches a failure state or success state"""
-        self.defer(
-            timeout=timedelta(seconds=self.timeout),
-            trigger=WasbBlobSensorTrigger(
-                container_name=self.container_name,
-                blob_name=self.blob_name,
-                wasb_conn_id=self.wasb_conn_id,
-                public_read=self.public_read,
-                poke_interval=self.poke_interval,
-            ),
-            method_name="execute_complete",
-        )
+        if not self.poke(context):
+            self.defer(
+                timeout=timedelta(seconds=self.timeout),
+                trigger=WasbBlobSensorTrigger(
+                    container_name=self.container_name,
+                    blob_name=self.blob_name,
+                    wasb_conn_id=self.wasb_conn_id,
+                    public_read=self.public_read,
+                    poke_interval=self.poke_interval,
+                ),
+                method_name="execute_complete",
+            )
 
     def execute_complete(self, context: Context, event: Dict[str, str]) -> None:
         """
