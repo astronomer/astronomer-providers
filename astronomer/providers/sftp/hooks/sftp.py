288b2cb3a1afd770f7154adc18a9e2ae575bceb2
Pankaj Koti <pankajkoti699@gmail.com>
2023-05-18 17:15:53 +0530

    Pass connection extra host_key as byte string known_host to asyncssh client
    With this, we do not need an additional paramiko validation, so
    also cleans up that code.

--- astronomer/providers/sftp/hooks/sftp.py
+++ astronomer/providers/sftp/hooks/sftp.py
@@ -1,20 +1,15 @@
 from __future__ import annotations
 
 import os.path
-from base64 import decodebytes
 from datetime import datetime
 from fnmatch import fnmatch
-from typing import Any, Sequence
+from typing import Sequence
 
 import asyncssh
-import paramiko
 from airflow.exceptions import AirflowException
 from airflow.hooks.base import BaseHook
 from airflow.models.connection import Connection
 from asgiref.sync import sync_to_async
-from paramiko.config import SSH_PORT
-from paramiko.ssh_exception import BadHostKeyException, SSHException
-from tenacity import RetryCallState, Retrying, stop_after_attempt, wait_fixed, wait_random
 
 
 class SFTPHookAsync(BaseHook):
@@ -38,13 +33,6 @@ class SFTPHookAsync(BaseHook):
     hook_name = "SFTP"
     default_known_hosts = "~/.ssh/known_hosts"
 
-    _host_key_mappings = {
-        "rsa": paramiko.RSAKey,
-        "dss": paramiko.DSSKey,
-        "ecdsa": paramiko.ECDSAKey,
-        "ed25519": paramiko.Ed25519Key,
-    }
-
     def __init__(  # nosec: B107
         self,
         sftp_conn_id: str = default_conn_name,
@@ -66,8 +54,6 @@ class SFTPHookAsync(BaseHook):
         self.key_file = key_file
         self.passphrase = passphrase
         self.private_key = private_key
-        self.no_host_key_check = False
-        self.host_key = None
 
     def _parse_extras(self, conn: Connection) -> None:
         """Parse extra fields from the connection into instance fields"""
@@ -88,22 +74,30 @@ class SFTPHookAsync(BaseHook):
             no_host_key_check = str(no_host_key_check).lower() == "true"
             if host_key is not None and no_host_key_check:
                 raise ValueError("Must check host key when provided.")
-            self.no_host_key_check = no_host_key_check
+            if no_host_key_check:
+                self.log.warning(
+                    "No Host Key Verification. This won't protect against Man-In-The-Middle attacks"
+                )
+                self.known_hosts = "none"
 
         if host_key is not None:
-            if host_key.startswith("ssh-"):
-                key_type, host_key = host_key.split(None)[:2]
-                key_constructor = self._host_key_mappings[key_type[4:]]
-            elif host_key.startswith("ecdsa-"):
-                _, host_key = host_key.split(None)[:2]
-                key_constructor = paramiko.ECDSAKey
-            else:
-                key_constructor = paramiko.RSAKey
-            decoded_host_key = decodebytes(host_key.encode("utf-8"))
-            self.host_key = key_constructor(data=decoded_host_key)
+            self.known_hosts = f"{conn.host} {host_key}".encode()
 
-    async def _get_asyncssh_client_connection(self, conn: Connection) -> asyncssh.SSHClientConnection:
-        """Return an asyncssh client connection object for the given connection"""
+    async def _get_conn(self) -> asyncssh.SSHClientConnection:
+        """
+        Asynchronously connect to the SFTP server as an SSH client
+
+        The following parameters are provided either in the extra json object in
+        the SFTP connection definition
+
+        - key_file
+        - known_hosts
+        - passphrase
+        """
+        conn = await sync_to_async(self.get_connection)(self.sftp_conn_id)
+        if conn.extra is not None:
+            self._parse_extras(conn)
+
         conn_config = {
             "host": conn.host,
             "port": conn.port,
@@ -124,91 +118,6 @@ class SFTPHookAsync(BaseHook):
             conn_config.update(passphrase=self.passphrase)
         ssh_client_conn = await asyncssh.connect(**conn_config)
         return ssh_client_conn
-
-    async def _get_conn(self) -> asyncssh.SSHClientConnection:
-        """
-        Asynchronously connect to the SFTP server as an SSH client
-
-        The following parameters are provided either in the extra json object in
-        the SFTP connection definition
-
-        - key_file
-        - known_hosts
-        - passphrase
-        """
-        conn = await sync_to_async(self.get_connection)(self.sftp_conn_id)
-        if conn.extra is not None:
-            self._parse_extras(conn)
-
-        if self.no_host_key_check:
-            self.log.warning("No Host Key Verification. This won't protect against Man-In-The-Middle attacks")
-            self.known_hosts = "none"
-        else:
-            self._validate_host_key_using_paramiko(conn)
-
-        ssh_client_conn = await self._get_asyncssh_client_connection(conn)
-        return ssh_client_conn
-
-    def _validate_host_key_using_paramiko(self, conn: Connection) -> None:
-        """
-        The asyncssh client we use does not support host key verification other than the ones given in
-        known_hosts file(Ref: https://github.com/ronf/asyncssh/issues/176). Hence, we use a paramiko client to
-        validate host keys before proceeding for further operation.
-        """
-        with paramiko.SSHClient() as paramiko_client:
-            connect_kwargs: dict[str, Any] = {
-                "hostname": conn.host,
-                "username": conn.login,
-                "password": conn.password,
-                "port": conn.port,
-            }
-            if self.private_key:
-                connect_kwargs.update(pkey=self.private_key)
-            if self.key_file:
-                connect_kwargs.update(key_filename=self.key_file)
-
-            paramiko_client.load_host_keys(filename=self.known_hosts)
-            client_host_keys = paramiko_client.get_host_keys()
-            if self.host_key:
-                if conn.port == SSH_PORT:
-                    client_host_keys.add(conn.host, self.host_key.get_name(), self.host_key)
-                else:
-                    client_host_keys.add(
-                        f"[{conn.host}]:{conn.port}", self.host_key.get_name(), self.host_key
-                    )
-                # We need to save the provided host_key back to known_hosts file as the AsyncSSH client we use can read
-                # keys from that file only. If the key is already present, it will be overwritten.
-                client_host_keys.save(self.known_hosts)
-
-            def _log_before_sleep(retry_state: RetryCallState) -> None:
-                return self.log.info(
-                    "Failed to connect. Sleeping before retry attempt %d", retry_state.attempt_number
-                )
-
-            for attempt in Retrying(
-                reraise=True,
-                wait=wait_fixed(3) + wait_random(0, 2),
-                stop=stop_after_attempt(3),
-                before_sleep=_log_before_sleep,
-            ):
-                with attempt:
-                    try:
-                        paramiko_client.connect(**connect_kwargs)
-                    except (BadHostKeyException, SSHException):
-                        # Reraise with less information as the BadHostKeyException includes the expected host key when
-                        # validating host keys.
-                        error_message = ""
-                        if self.host_key:
-                            error_message = (
-                                f"\nGiven host key '{self.host_key.get_name()} {self.host_key.get_base64()}' for "
-                                f"server '{conn.host}' does not match!\n"
-                            )
-                        error_message += (
-                            f"None of the host keys in the 'known_hosts' file '{self.known_hosts}' match!"
-                        )
-                        raise SSHException(error_message)
-
-        self.log.info("Validated host key successfully. Proceeding with further operations...")
 
     async def list_directory(self, path: str = "") -> list[str] | None:
         """Returns a list of files on the SFTP server at the provided path"""
