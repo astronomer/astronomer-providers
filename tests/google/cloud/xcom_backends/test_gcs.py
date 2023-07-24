27626d750852769f14ffec2c7d278d11c9323729
Pankaj Koti <pankaj.koti@astronomer.io>
2023-07-24 17:59:25 +0530

    Correct testing module import path as per Pandas 2.0.0 release (#1253)
    Tests have been failing as observed in PR #1252
    for the missing import path pandas.util.testing. It is observed
    the module was moved one level up in the Pandas 2.0.0 release
    and hence import the module from that path.

--- tests/google/cloud/xcom_backends/test_gcs.py
+++ tests/google/cloud/xcom_backends/test_gcs.py
@@ -13,7 +13,7 @@ import pytest
 from airflow import settings
 from airflow.configuration import conf
 from airflow.models.xcom import BaseXCom
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 
 from astronomer.providers.google.cloud.xcom_backends.gcs import (
     GCSXComBackend,
