a95e91a3f25bc0990315a8d6f75a5c0a42817096
Pankaj <pankaj.singh@astronomer.io>
2023-08-07 10:29:58 +0530

    Apply review suggestions

--- astronomer/providers/utils/sensor_util.py
+++ astronomer/providers/utils/sensor_util.py
@@ -9,18 +9,18 @@ if TYPE_CHECKING:  # pragma: no cover
     from astronomer.providers.utils.typing_compat import Context
 
 
-def poke(cls: BaseSensorOperator, context: Context) -> bool:  # pragma: no cover
+def poke(cls: BaseSensorOperator, context: Context) -> bool:
     """Wrapper to call the sensor method"""
     try:
         return cls.poke(context)  # type: ignore[return-value]
     except Exception as e:
         if cls.soft_fail:
             raise AirflowSkipException(str(e))
-        else:
+        else:  # pragma: no cover
             raise e
 
 
-def raise_error_or_skip_exception(soft_fail: bool, error_message: str) -> None:  # pragma: no cover
+def raise_error_or_skip_exception(soft_fail: bool, error_message: str) -> None:
     """Raise error based on soft_fail flag"""
     if soft_fail:
         raise AirflowSkipException(error_message)
