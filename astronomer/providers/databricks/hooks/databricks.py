8f141b27f79fc8fc31031c046ed37cdcfae0665d
Ryan Hatter <25823361+RNHTTR@users.noreply.github.com>
2023-10-09 16:54:39 -0400

    fix mypy

--- astronomer/providers/databricks/hooks/databricks.py
+++ astronomer/providers/databricks/hooks/databricks.py
@@ -3,7 +3,7 @@ import base64
 from typing import Any, Dict, Tuple, cast
 
 import aiohttp
-from aiohttp import ClientConnectorError, ClientResponseError
+from aiohttp import ClientConnectorError, ClientError, ClientResponseError
 from airflow import __version__
 from airflow.exceptions import AirflowException
 from airflow.providers.databricks.hooks.databricks import (
@@ -150,7 +150,7 @@ class DatabricksHookAsync(DatabricksHook):
                 attempt_num += 1
                 await asyncio.sleep(self.retry_delay)
 
-    def _retryable_error_async(self, exception: ClientResponseError) -> bool:
+    def _retryable_error_async(self, exception: ClientError) -> bool:
         """
         Determines whether or not an exception that was thrown might be successful
         on a subsequent attempt.
