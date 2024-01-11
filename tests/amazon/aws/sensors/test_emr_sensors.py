244a501390d7a374197195a54b07c4b20145946a
Wei Lee <weilee.rx@gmail.com>
2024-01-11 19:15:25 +0800

    feat(amazon): deprecate EmrJobFlowSensorAsync

--- tests/amazon/aws/sensors/test_emr_sensors.py
+++ tests/amazon/aws/sensors/test_emr_sensors.py
@@ -1,19 +1,12 @@
-from unittest import mock
-from unittest.mock import PropertyMock
 
-import pytest
-from airflow.exceptions import AirflowException, TaskDeferred
 from airflow.providers.amazon.aws.sensors.emr import (
+    EmrContainerSensor,
     EmrStepSensor,
 )
 
 from astronomer.providers.amazon.aws.sensors.emr import (
     EmrContainerSensorAsync,
-    EmrJobFlowSensorAsync,
     EmrStepSensorAsync,
-)
-from astronomer.providers.amazon.aws.triggers.emr import (
-    EmrJobFlowSensorTrigger,
 )
 
 TASK_ID = "test_emr_container_sensor"
@@ -38,7 +31,7 @@ class TestEmrContainerSensorAsync:
             max_retries=1,
             aws_conn_id=AWS_CONN_ID,
         )
-        assert isinstance(task, EmrContainerSensorAsync)
+        assert isinstance(task, EmrContainerSensor)
         assert task.deferrable is True
 
 
@@ -54,63 +47,10 @@ class TestEmrStepSensorAsync:
 
 
 class TestEmrJobFlowSensorAsync:
-    TASK = EmrJobFlowSensorAsync(
-        task_id=TASK_ID,
-        job_flow_id=JOB_ID,
-    )
-
-    @mock.patch(f"{MODULE}.EmrJobFlowSensorAsync.defer")
-    @mock.patch(f"{MODULE}.EmrJobFlowSensorAsync.hook", new_callable=PropertyMock)
-    def test_emr_job_flow_sensor_async_finish_before_deferred(self, mock_hook, mock_defer, context):
-        """Assert task is not deferred when it receives a finish status before deferring"""
-        mock_hook.return_value.conn.describe_cluster.return_value = {
-            "Cluster": {"Status": {"State": "TERMINATED"}}
-        }
-        self.TASK.execute(context)
-        assert not mock_defer.called
-
-    @mock.patch(f"{MODULE}.EmrJobFlowSensorAsync.defer")
-    @mock.patch(f"{MODULE}.EmrJobFlowSensorAsync.hook", new_callable=PropertyMock)
-    def test_emr_job_flow_sensor_async_failed_before_deferred(self, mock_hook, mock_defer, context):
-        """Assert task is not deferred when it receives a finish status before deferring"""
-        mock_hook.return_value.conn.describe_cluster.return_value = {
-            "Cluster": {"Status": {"State": "TERMINATED_WITH_ERRORS"}}
-        }
-        with pytest.raises(AirflowException):
-            self.TASK.execute(context)
-        assert not mock_defer.called
-
-    @pytest.mark.parametrize("status", ("STARTING", "BOOTSTRAPPING", "RUNNING", "WAITING"))
-    @mock.patch(f"{MODULE}.EmrJobFlowSensorAsync.hook", new_callable=PropertyMock)
-    def test_emr_job_flow_sensor_async(self, mock_hook, status, context):
-        """
-        Asserts that a task is deferred and a EmrJobFlowSensorTrigger will be fired
-        when the EmrJobFlowSensorAsync is executed.
-        """
-        mock_hook.return_value.conn.describe_cluster.return_value = {"Cluster": {"Status": {"State": status}}}
-        with pytest.raises(TaskDeferred) as exc:
-            self.TASK.execute(context)
-        assert isinstance(
-            exc.value.trigger, EmrJobFlowSensorTrigger
-        ), "Trigger is not a EmrJobFlowSensorTrigger"
-
-    def test_emr_flow_sensor_async_execute_failure(self, context):
-        """Test EMR flow sensor with an AirflowException is raised in case of error event"""
-
-        with pytest.raises(AirflowException):
-            self.TASK.execute_complete(
-                context=None, event={"status": "error", "message": "test failure message"}
-            )
-
-    def test_emr_job_flow_sensor_async_execute_complete(self):
-        """Asserts that logging occurs as expected"""
-
-        assert (
-            self.TASK.execute_complete(context=None, event={"status": "success", "message": "Job completed"})
-            is None
+    def test_init(self):
+        task = EmrStepSensorAsync(
+            task_id=TASK_ID,
+            job_flow_id=JOB_ID,
         )
-
-    def test_emr_job_flow_sensor_async_execute_complete_event_none(self):
-        """Asserts that logging occurs as expected"""
-
-        assert self.TASK.execute_complete(context=None, event=None) is None
+        assert isinstance(task, EmrStepSensor)
+        assert task.deferrable is True
