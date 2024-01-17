b697ecc27eb945d57d884183bd22dda57bd264cb
Wei Lee <weilee.rx@gmail.com>
2024-01-17 15:30:05 +0800

    Deprecate EmrJobFlowSensorAsync (#1406)
    * feat(amazon): deprecate EmrJobFlowSensorAsync
    * feat(amazon): fallback poll_interval to poke_interval for EmrJobFlowSensorAsync
    * feat(amazon): add deprecation warning to emr hooks and triggerer

--- astronomer/providers/amazon/aws/sensors/emr.py
+++ astronomer/providers/amazon/aws/sensors/emr.py
@@ -1,22 +1,12 @@
 from __future__ import annotations
 
 import warnings
-from datetime import timedelta
-from typing import Any
 
-from airflow import AirflowException
-from airflow.exceptions import AirflowSkipException
 from airflow.providers.amazon.aws.sensors.emr import (
     EmrContainerSensor,
     EmrJobFlowSensor,
     EmrStepSensor,
 )
-
-from astronomer.providers.amazon.aws.triggers.emr import (
-    EmrJobFlowSensorTrigger,
-)
-from astronomer.providers.utils.sensor_util import raise_error_or_skip_exception
-from astronomer.providers.utils.typing_compat import Context
 
 
 class EmrContainerSensorAsync(EmrContainerSensor):
@@ -26,6 +16,16 @@ class EmrContainerSensorAsync(EmrContainerSensor):
     """
 
     def __init__(self, *args, **kwargs) -> None:  # type: ignore[no-untyped-def]
+        poll_interval = kwargs.pop("poll_interval")
+        if poll_interval:
+            self.poke_interval = poll_interval
+            warnings.warn(
+                "Argument `poll_interval` is deprecated and will be removed "
+                "in a future release.  Please use  `poke_interval` instead.",
+                DeprecationWarning,
+                stacklevel=2,
+            )
+
         warnings.warn(
             (
                 "This module is deprecated. "
@@ -59,65 +59,18 @@ class EmrStepSensorAsync(EmrStepSensor):
 
 class EmrJobFlowSensorAsync(EmrJobFlowSensor):
     """
-    Async EMR Job flow sensor polls for the cluster state until it reaches
-    any of the target states.
-    If it fails the sensor errors, failing the task.
-    With the default target states, sensor waits cluster to be terminated.
-    When target_states is set to ['RUNNING', 'WAITING'] sensor waits
-    until job flow to be ready (after 'STARTING' and 'BOOTSTRAPPING' states)
-
-    :param job_flow_id: job_flow_id to check the state of cluster
-    :param target_states: the target states, sensor waits until
-        job flow reaches any of these states
-    :param failed_states: the failure states, sensor fails when
-        job flow reaches any of these states
+    This class is deprecated.
+    Please use :class: `~airflow.providers.amazon.aws.sensors.emr.EmrJobFlowSensor`.
     """
 
-    def __init__(
-        self,
-        *,
-        poll_interval: float = 5,
-        **kwargs: Any,
-    ):
-        self.poll_interval = poll_interval
-        super().__init__(**kwargs)
-
-    def execute(self, context: Context) -> None:
-        """Defers trigger class to poll for state of the job run until it reaches a failure state or success state"""
-        emr_client = self.hook.conn
-        self.log.info("Poking cluster %s", self.job_flow_id)
-        response = emr_client.describe_cluster(ClusterId=self.job_flow_id)
-        state = response["Cluster"]["Status"]["State"]
-        self.log.info("Job flow currently %s", state)
-
-        if state == "TERMINATED":
-            return None
-
-        if state == "TERMINATED_WITH_ERRORS":
-            if self.soft_fail:  # pragma: no cover
-                AirflowSkipException(f"EMR job failed: {self.failure_message_from_response(response)}")
-            raise AirflowException(f"EMR job failed: {self.failure_message_from_response(response)}")
-
-        self.defer(
-            timeout=timedelta(seconds=self.timeout),
-            trigger=EmrJobFlowSensorTrigger(
-                job_flow_id=self.job_flow_id,
-                aws_conn_id=self.aws_conn_id,
-                target_states=self.target_states,
-                failed_states=self.failed_states,
-                poll_interval=self.poll_interval,
+    def __init__(self, *args, **kwargs) -> None:  # type: ignore[no-untyped-def]
+        warnings.warn(
+            (
+                "This module is deprecated. "
+                "Please use `airflow.providers.amazon.aws.sensors.emr.EmrJobFlowSensor` "
+                "and set deferrable to True instead."
             ),
-            method_name="execute_complete",
+            DeprecationWarning,
+            stacklevel=2,
         )
-
-    def execute_complete(self, context: Context, event: dict[str, str]) -> None:  # type: ignore[override]
-        """
-        Callback for when the trigger fires - returns immediately.
-        Relies on trigger to throw an exception, otherwise it assumes execution was
-        successful.
-        """
-        if event:
-            if event["status"] == "error":
-                raise_error_or_skip_exception(self.soft_fail, event["message"])
-            self.log.info(event["message"])
-        return None
+        return super().__init__(*args, deferrable=True, **kwargs)
