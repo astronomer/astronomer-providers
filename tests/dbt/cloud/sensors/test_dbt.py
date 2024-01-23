b0edbd9295cecbe3dc26cc08589b798ecf9d7033
Pankaj Koti <pankajkoti699@gmail.com>
2024-01-23 18:35:24 +0530

    Add test for poll_interval deprecation warning

--- tests/dbt/cloud/sensors/test_dbt.py
+++ tests/dbt/cloud/sensors/test_dbt.py
@@ -1,3 +1,4 @@
+import pytest
 from airflow.providers.dbt.cloud.sensors.dbt import DbtCloudJobRunSensor
 
 from astronomer.providers.dbt.cloud.sensors.dbt import DbtCloudJobRunSensorAsync
@@ -19,3 +20,15 @@ class TestDbtCloudJobRunSensorAsync:
 
         assert isinstance(task, DbtCloudJobRunSensor)
         assert task.deferrable is True
+
+    def test_poll_interval_deprecation_warning(self):
+        """Test DeprecationWarning for DbtCloudJobRunSensorAsync by setting param poll_interval"""
+        # TODO: Remove once deprecated
+        with pytest.warns(expected_warning=DeprecationWarning):
+            DbtCloudJobRunSensorAsync(
+                dbt_cloud_conn_id=self.CONN_ID,
+                task_id=self.TASK_ID,
+                run_id=self.DBT_RUN_ID,
+                timeout=self.TIMEOUT,
+                poll_interval=5.0,
+            )
