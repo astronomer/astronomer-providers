4cbc39ec3614e70ca1004d19c20c54021e00dc51
vatsrahul1001 <rah.sharma11@gmail.com>
2024-08-16 11:39:51 +0530

    add databricks workflow dag to master dag

--- .github/scripts/refactor_dag.py
+++ .github/scripts/refactor_dag.py
@@ -2,15 +2,17 @@ from __future__ import annotations
 
 import argparse
 
+
 def remove_lines_after(file_path, target_line):
-    with open(file_path, 'r') as input_file:
+    with open(file_path) as input_file:
         lines = input_file.readlines()
 
-    with open(file_path, 'w') as output_file:
+    with open(file_path, "w") as output_file:
         for line in lines:
             if target_line in line:
                 break
             output_file.write(line)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
