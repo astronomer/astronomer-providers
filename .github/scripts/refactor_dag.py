41b94f62875f1f5e0ce59142a11a07d272951591
vatsrahul1001 <rah.sharma11@gmail.com>
2024-10-12 13:42:21 +0530

    fix typo

--- .github/scripts/refactor_dag.py
+++ .github/scripts/refactor_dag.py
@@ -28,5 +28,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("file_path", help="file path", type=str)
     args = parser.parse_args()
-    target_line = "from dev.tests_common.test_utils.watcher import watcherr"
+    target_line = "from dev.tests_common.test_utils.watcher import watcher"
     remove_lines_after(args.file_path, target_line)
